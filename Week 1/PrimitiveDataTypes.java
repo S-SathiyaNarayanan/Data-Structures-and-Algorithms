d6eebc70e891c583104f704087ae4702a72a03e2
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-07 12:07:37 +0530

    Create PrimitiveDataTypes.java

---
+++ Week 1/PrimitiveDataTypes.java
@@ -0,0 +1,23 @@
+public class PrimitiveDataTypes{
+    public static void main(String[] args){
+        // Declare and initialize primitive variables 
+        byte myByteValue = 100;
+        short myShortValue = 5000;
+        int myIntValue = 100000;
+        long myLongValue = 15000000000L;
+        float myFloatValue = 5.75f;
+        double myDoubleValue = 19.99;
+        char myCharValue = 'A';
+        boolean myBooleanValue = true;
+
+        // Print all primitive values
+        System.out.println("Byte value: " + myByteValue);
+        System.out.println("Short value: " + myShortValue);
+        System.out.println("Int value: " + myIntValue);
+        System.out.println("Long value: " + myLongValue);
+        System.out.println("Float value: " + myFloatValue);
+        System.out.println("Double value: " + myDoubleValue);
+        System.out.println("Char value: " + myCharValue);
+        System.out.println("Boolean value: " + myBooleanValue);
+    }
+}
