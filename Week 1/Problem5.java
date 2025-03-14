4d5ba083e51148ee28efd025c7cbd53b492c4371
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-14 23:04:20 +0530

    Create Problem5.java

---
+++ Week 1/Problem5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Problem5 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        
+        for (int i = 0; i < y; i++) {
+            System.out.println(x);
+        }
+    }
+}
