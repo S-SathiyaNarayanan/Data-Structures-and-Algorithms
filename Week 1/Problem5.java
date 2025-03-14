86effc35f1c80a41a9f5a1dceb6d7a620b8a1b93
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-14 23:12:51 +0530

    Update Problem5.java

--- Week 1/Problem5.java
+++ Week 1/Problem5.java
@@ -1,3 +1,14 @@
+/*************************************************************************************************************
+Write a program which takes two values x and y. Prints x for y number of times.
+Input:
+2 
+3
+Expected Output:
+2
+2
+2
+*************************************************************************************************************/
+
 import java.util.Scanner;
 
 public class Problem5 {
