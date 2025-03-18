a0b782832632af40ce69a5ece173be5e1e3815bb
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-18 23:27:32 +0530

    Create ForLoop.java

---
+++ Week 1/ForLoop.java
@@ -0,0 +1,10 @@
+public class ForLoop {
+    public static void main(String[] args) {
+
+        // For loop that runs from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Iteration " + i);
+        }
+
+    }
+}
