e44634756d94c9b155ba98c11b0d7ab60022e7e2
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-19 00:44:19 +0530

    Create WhileLoop.java

---
+++ Week 1/WhileLoop.java
@@ -0,0 +1,13 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 1; 
+
+        // While loop runs as long as count is less than or equal to 5
+        while (count <= 5) {
+            System.out.println("Current Count: " + count);
+            count++; // Increment counter
+        }
+        
+        System.out.println("\nLoop Finished!");
+    }
+}
