ab0801b2b55b2562fff5c6e6f86016161a264743
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-18 20:35:57 +0530

    Update SwitchStatement.java

--- Week 1/SwitchStatement.java
+++ Week 1/SwitchStatement.java
@@ -2,11 +2,14 @@ import java.util.Scanner;
 
 public class SwitchStatement {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
-
-        // Prompt the user to enter a day number
         System.out.print("Enter a day number (1-7): ");
         int day = scanner.nextInt();
+        
+        // Use a switch when you have one variable and need to check it against multiple fixed values using exact matches
+        // Use switch with: byte, short, char, int, String (Java 7+), and enum (Java 5+)
+        // Avoid switch with: long, float, double, boolean, and non-String objects
 
         // Switch statement to determine and print the day name
         switch (day) {
