47914c7d6b9ed6c56345fe65e6861d36dcbff019
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-19 05:43:16 +0530

    Update ClassAndObjects.java

--- Week 2/ClassAndObjects.java
+++ Week 2/ClassAndObjects.java
@@ -4,23 +4,22 @@ class Human {
     String name;       // Name of the person
     String gender;     // Gender of the person
     int age;           // Age of the person
-    String height;     // Height of the person (in meters)
-    String weight;     // Weight of the person (in kilograms)
+    double height;     // Height of the person (in meters, e.g. 1.66)
+    double weight;     // Weight of the person (in kilograms, e.g. 69.0)
     String occupation; // Occupation of the person
 
     // Constructor to initialize the object with values for all attributes
-    public Human(String name, String gender, int age, String height, String weight, String occupation) {
-        this.name = name;           // Set name
-        this.gender = gender;       // Set gender
-        this.age = age;             // Set age
-        this.height = height;       // Set height
-        this.weight = weight;       // Set weight
-        this.occupation = occupation; // Set occupation
+    public Human(String name, String gender, int age, double height, double weight, String occupation) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.height = height;   // Expecting a numeric value like 1.66
+        this.weight = weight;   // Expecting a numeric value like 69.0
+        this.occupation = occupation;
     }
 
-    // Method to display the details of the human (attributes)
+    // Method to display the details of the person
     public void displayDetails() {
-        // Printing the details of the person
         System.out.println("Name: " + name);
         System.out.println("Gender: " + gender);
         System.out.println("Age: " + age);
@@ -29,20 +28,15 @@ class Human {
         System.out.println("Occupation: " + occupation);
     }
 
-    // Method with arguments and parameters: A simple function to calculate BMI
+    // Method to calculate and display the BMI of the person
     public void calculateBMI() {
-        // Convert height and weight to numeric values for BMI calculation
-        double heightInMeters = Double.parseDouble(height.replace("m", "")); // Remove "m" and convert to double
-        double weightInKg = Double.parseDouble(weight.replace("kg", ""));    // Remove "kg" and convert to double
-
-        // BMI Formula: BMI = weight (kg) / (height (m) * height (m))
-        double bmi = weightInKg / (heightInMeters * heightInMeters);
-        System.out.println("BMI: " + bmi); // Output the calculated BMI
+        // BMI formula: weight / (height * height)
+        double bmi = weight / (height * height);
+        System.out.println("BMI: " + bmi);
     }
 
-    // Method with a return value: A simple function to get a description of the person
+    // Method to get a description of the person
     public String getDescription() {
-        // Return a description string about the person
         return name + " is a " + age + " years old " + gender + " who works as a " + occupation + ".";
     }
 }
@@ -51,18 +45,18 @@ class Human {
 public class ClassAndObjects {
     public static void main(String[] args) {
         // Creating objects (instances of the Human class)
-        Human sathiya = new Human("Sathiya Narayanan", "Male", 24, "1.66m", "69kg", "Software Engineer");
-        Human gamana = new Human("Gamana Suresh", "Female", 24, "1.66m", "45kg", "Data Analyst");
+        Human sathiya = new Human("Sathiya Narayanan", "Male", 24, 1.66, 69.0, "Software Engineer");
+        Human gamana = new Human("Gamana Suresh", "Female", 24, 1.66, 45.0, "Data Analyst");
 
-        // Calling methods on objects to display details
+        // Displaying details and calculating BMI for the first object
         System.out.println("Object-1 Details:");
-        sathiya.displayDetails();    // Display details of 'sathiya'
-        sathiya.calculateBMI();      // Calculate and display BMI of 'sathiya'
-        System.out.println("Description: " + sathiya.getDescription()); // Get and display description of 'sathiya'
+        sathiya.displayDetails();
+        sathiya.calculateBMI();
+        System.out.println("Description: " + sathiya.getDescription());
 
         System.out.println("\nObject-2 Details:");
-        gamana.displayDetails();    // Display details of 'gamana'
-        gamana.calculateBMI();      // Calculate and display BMI of 'gamana'
-        System.out.println("Description: " + gamana.getDescription()); // Get and display description of 'gamana'
+        gamana.displayDetails();
+        gamana.calculateBMI();
+        System.out.println("Description: " + gamana.getDescription());
     }
 }
