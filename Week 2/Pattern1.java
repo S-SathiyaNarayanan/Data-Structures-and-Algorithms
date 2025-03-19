296288bc359f26bb98347804c14f4c3e4d3a5d3e
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-19 11:52:24 +0530

    Update Pattern1.java

--- Week 2/Pattern1.java
+++ Week 2/Pattern1.java
@@ -3,13 +3,13 @@ public class Pattern1{
     public static void main(String[] args){
       
         Scanner sc = new Scanner(System.in);
-        int inner = sc.nextInt();    // number of rows to print
-        int outer = sc.nextInt();    // number of stars to print in each row
+        int NumOfRows = sc.nextInt();           // number of rows to print
+        int NumOfStarsPerRow = sc.nextInt();    // number of stars to print in each row
         
         // Outer loop: controls the number of rows in the pattern
-        for(int i = 1; i <= inner; i++){
+        for(int i = 1; i <= NumOfRows; i++){
             // Inner loop: prints number of stars for the current row
-            for(int j = 1; j <= outer; j++){
+            for(int j = 1; j <= NumOfStarsPerRow; j++){
                 System.out.print("* ");    // Print a star followed by a space
             }
             // Move to the next line after completing all columns in the current row
