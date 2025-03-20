16dcded5e2abf4939db333de8b2e380ad5885c4d
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-20 11:19:22 +0530

    Update Pattern4.java

--- Week 2/Pattern4.java
+++ Week 2/Pattern4.java
@@ -1,1 +1,23 @@
-
+/*************************************************************************************************************
+🪙GeeksforGeeks
+Given a number N. The task is to print a series of asterisk(*) from 1 till N terms with increasing order and 
+difference being 1.
+Input:
+N = 3
+Output:
+* ** ***
+Explanation:
+First, print 1 asterisk then space after that print 2 asterisk and space after that  print 3 asterisk now 
+stop as N is 3.
+*************************************************************************************************************/
+public class Pattern4{
+    public static void main(String[] args){
+        int N=3;
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.print(" ");
+        }
+    }
+}
