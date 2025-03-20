a025ed980a4bcc33307a59fb30bcfedff853a81b
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-20 11:22:05 +0530

    Update Pattern4.java

--- Week 2/Pattern4.java
+++ Week 2/Pattern4.java
@@ -10,7 +10,8 @@ Explanation:
 First, print 1 asterisk then space after that print 2 asterisk and space after that  print 3 asterisk now 
 stop as N is 3.
 *************************************************************************************************************/
-public class Pattern4{
+// Solution 1
+public class Pattern4 {
     public static void main(String[] args){
         int N=3;
         for(int i=1;i<=N;i++){
@@ -21,3 +22,13 @@ public class Pattern4{
         }
     }
 }
+// Solution 2
+public class Patern4 {
+    public static void main(String[] args) {
+        int N = 3;
+        for (int i = 1; i <= N; i++) {
+            System.out.print("*".repeat(i) + " ");
+        }
+    }
+}
+
