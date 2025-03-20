aa6090ea248e8e77ce3d7b206b453a605cf47a75
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-21 03:51:16 +0530

    Update FindLastDigit.java

--- Week 2/FindLastDigit.java
+++ Week 2/FindLastDigit.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
 public class FindLastDigit{
     public static void main(String[] args){
+        
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the Number: ");
         int N=sc.nextInt();
-        int LastDigit=N%10;
+        
+        // When you divide a number by 10, the remainder is always the last digit of that number. 
+        // The `%` (modulus) operator gives that remainder, which is why `N % 10` extracts the last digit.
+        int LastDigit = N % 10;
         System.out.println("Last digit: "+LastDigit);
     }
 }
