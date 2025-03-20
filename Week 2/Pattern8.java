6e96477e059f1429452488eb867800d7d46513d8
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-20 20:13:24 +0530

    Create Pattern8.java

---
+++ Week 2/Pattern8.java
@@ -0,0 +1,25 @@
+/*************************************************************************************************************
+Pattern 8
+Write a Program that takes input n and prints the below pattern 
+Input:
+n = 4
+Expected output:
+*
+**
+***
+****
+
+*************************************************************************************************************/
+import java.util.Scanner;
+public class Pattern8{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
