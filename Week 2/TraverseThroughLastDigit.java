8eaff10aeb0b79f4f091a7f99c0d32130d6f1dd7
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-22 06:06:35 +0530

    Update TraverseThroughLastDigit.java

--- Week 2/TraverseThroughLastDigit.java
+++ Week 2/TraverseThroughLastDigit.java
@@ -5,13 +5,7 @@ public class TraverseThroughLastDigit {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the Number: ");
         int N = sc.nextInt();
-
-        if (N == 0) {        // Edge case for 0
-            System.out.println("Traversing through Last Digits");
-            System.out.println(0);
-            return;
-        }
-
+        
         System.out.println("Traversing through Last Digits");
         while (N > 0) {
             int LastDigit = N % 10;        // Extract last digit
