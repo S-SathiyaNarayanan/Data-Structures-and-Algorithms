1696392a9d443bced28b61e5961f3d919506c8f6
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-25 13:00:24 +0530

    Create Pattern9.java

---
+++ Week 2/Pattern9.java
@@ -0,0 +1,16 @@
+/*************************************************************************************************************
+🪙GeeksforGeeks - Pattern 10
+Geek is very fond of patterns. Once, his teacher gave him a pattern to solve. He gave Geek an integer n and 
+asked him to build a pattern. Help Geek to build a star pattern.
+Input: 5
+Output:
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+* * * *
+* * *
+* *
+*
+*************************************************************************************************************/
