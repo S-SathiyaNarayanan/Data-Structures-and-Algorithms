d8a4d9004f6f0f195c4820754ab395bab5ac2281
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-25 19:03:22 +0530

    Update PalindromeNumber.java

--- Week 3/PalindromeNumber.java
+++ Week 3/PalindromeNumber.java
@@ -1,2 +1,8 @@
-// LeetCode 9     
-// https://leetcode.com/problems/palindrome-number/description/
+/*************************************************************************************************************
+💎LeetCode 9 - Palindrome Number 
+🪙GeeksforGeeks - Palindrome
+You are given an integer N. Your task is to determine whether it is a palindrome.
+Input: N = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121- and it is not a palindrome
+*************************************************************************************************************/
