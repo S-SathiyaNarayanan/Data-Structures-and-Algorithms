0e3621b08614a09796e9b02297b5ee5f1ee8e4f6
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-25 19:19:37 +0530

    Update PalindromeNumber.java

--- Week 3/PalindromeNumber.java
+++ Week 3/PalindromeNumber.java
@@ -6,3 +6,24 @@ Input: N = -121
 Output: false
 Explanation: From left to right, it reads -121. From right to left, it becomes 121- and it is not a palindrome
 *************************************************************************************************************/
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        int N = 121; // Change this number to test different values
+        int OriginalNumber = N;
+        int ReversedNumber = 0;
+
+        while (N > 0) {
+            int digit = N % 10;  // Get the last digit
+            ReversedNumber = ReversedNumber * 10 + digit;  // Append it to reversed number
+            N = N / 10;  // Remove the last digit
+        }
+
+        if (OriginalNumber == ReversedNumber) {
+            System.out.println(OriginalNumber + " is a palindrome.");
+        } else {
+            System.out.println(OriginalNumber + " is not a palindrome.");
+        }
+        // Value of N becomes 0
+    }
+}
+
