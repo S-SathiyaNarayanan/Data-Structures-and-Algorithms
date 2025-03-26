01f1a436debb3fdd06f673f602a6da9b57de1697
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-26 14:20:50 +0530

    Update PrintElementsOfArray.java

--- Week 3/PrintElementsOfArray.java
+++ Week 3/PrintElementsOfArray.java
@@ -5,3 +5,15 @@ Input: arr[] = [1, 2, 3, 4, 5]
 Output: 1 2 3 4 5
 Note: You don't need to move to the next line after printing all elements of the array (space-separated)
 *************************************************************************************************************/
+
+// User function Template for Java
+class Solution {
+    // Just print the space seperated array elements
+    void printArray(int arr[]) {
+        //   code here
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
