45e0ba31feb468d45d2cac48a679372ff8e90c7d
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-27 05:14:44 +0530

    Update SumOfArrayElements.java

--- Week 4/SumOfArrayElements.java
+++ Week 4/SumOfArrayElements.java
@@ -5,3 +5,17 @@ Input: arr[] = [1, 2, 3, 4]
 Output: 10
 Explanation: 1 + 2 + 3 + 4 = 10.
 *************************************************************************************************************/
+
+// User function Template for Java
+class Solution {
+    int arraySum(int arr[]) {
+        // code here
+        int n=arr.length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum=sum+arr[i];
+        }
+        return sum;
+    }
+}
+
