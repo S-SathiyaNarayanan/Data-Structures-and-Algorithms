1433e57b0ffc60cf6f3c8e375275b5bdcc3d28dc
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-26 13:58:27 +0530

    Update PrintElementsOfArray.java

--- Week 3/PrintElementsOfArray.java
+++ Week 3/PrintElementsOfArray.java
@@ -1,1 +1,7 @@
-
+/*************************************************************************************************************
+🪙GeeksforGeeks - Print Elements of Array
+Given an array arr[], print all its elements space-separated.
+Input: arr[] = [1, 2, 3, 4, 5]
+Output: 1 2 3 4 5
+Note: You don't need to move to the next line after printing all elements of the array (space-separated)
+*************************************************************************************************************/
