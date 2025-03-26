5550b19cbb51a8096d634f67c363e05363ccf19e
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-26 11:14:02 +0530

    Update FactorialOfNumber.java

--- Week 3/FactorialOfNumber.java
+++ Week 3/FactorialOfNumber.java
@@ -1,3 +1,10 @@
+/*************************************************************************************************************
+🪙GeeksforGeeks - Factorial
+Given a positive integer, N. Find the factorial of N.
+Input: N = 5
+Output: 120
+Explanation: 1 x 2 x 3 x 4 x 5 = 120
+*************************************************************************************************************/
 import java.util.Scanner;    // Import Scanner class for user input
 public class FactorialOfNumber {
     public static void main(String[] args) {
