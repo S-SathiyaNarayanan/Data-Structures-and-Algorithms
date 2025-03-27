d64a079250d7171aad6b467d2973a4b12bc097e4
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-27 06:26:10 +0530

    Update CountOddAndEven.java

--- Week 4/CountOddAndEven.java
+++ Week 4/CountOddAndEven.java
@@ -6,3 +6,22 @@ Input: arr[] = [1, 2, 3, 4, 5]
 Output: 3 2
 Explanation: There are 3 odd elements (1, 3, 5) and 2 even elements (2 and 4).
 *************************************************************************************************************/
+
+// User function Template for Java
+class Solution {
+    public int[] countOddEven(int[] arr) {
+        // Code here
+        int n=arr.length;
+        int even=0;
+        int odd=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]%2==0){
+                even=even+1;
+            }
+            else{
+                odd=odd+1;
+            }
+        }
+        return new int[]{odd,even};
+    }
+}
