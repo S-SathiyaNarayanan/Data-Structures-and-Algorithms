2c33f932e1ef45685b1ac1f8305016a29e831cd0
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-27 05:45:10 +0530

    Create AlternatesInAnArray.java

---
+++ Week 4/AlternatesInAnArray.java
@@ -0,0 +1,12 @@
+/*************************************************************************************************************
+🪙GeeksforGeeks - Alternates in an Array
+You are given an array arr[], the task is to return a list elements of arr in alternate order. Starting from 
+index 0.
+Input: arr[] = [1, 2, 3, 4]
+Output: 1 3
+Explanation:
+Take first element: 1
+Skip second element: 2
+Take third element: 3
+Skip fourth element: 4
+*************************************************************************************************************/
