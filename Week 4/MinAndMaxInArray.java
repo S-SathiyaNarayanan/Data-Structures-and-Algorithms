1a85fe512dfdbbe8fc07563c3324571f132c8fd8
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-27 05:40:18 +0530

    Create MinAndMaxInArray.java

---
+++ Week 4/MinAndMaxInArray.java
@@ -0,0 +1,7 @@
+/*************************************************************************************************************
+🪙GeeksforGeeks - Min and Max in Array
+Given an array arr. Your task is to find the minimum and maximum elements in the array.
+Input: arr[] = [3, 2, 1, 56, 10000, 167]
+Output: 1 10000
+Explanation: minimum and maximum elements of array are 1 and 10000.
+*************************************************************************************************************/
