169eb62f8b3a868d6f8296c048d59e9cfabac80f
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-27 05:30:10 +0530

    Create FirstAndSecondSmallest.java

---
+++ Week 4/FirstAndSecondSmallest.java
@@ -0,0 +1,8 @@
+/*************************************************************************************************************
+🪙GeeksforGeeks - First and Second Smallests
+Given an array, arr of integers, your task is to return the smallest and second smallest element in the array.
+If the smallest and second smallest do not exist, return -1.
+Input: arr[] = [2, 4, 3, 5, 6]
+Output: 2 3 
+Explanation: 2 and 3 are respectively the smallest and second smallest elements in the array.
+*************************************************************************************************************/
