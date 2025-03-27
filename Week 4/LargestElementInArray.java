bb4dc8cf3c3eb22f641c87d884f65f6b6d1fb656
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-27 11:53:28 +0530

    Update LargestElementInArray.java

--- Week 4/LargestElementInArray.java
+++ Week 4/LargestElementInArray.java
@@ -5,3 +5,17 @@ Input: arr[] = [1, 8, 7, 56, 90]
 Output: 90
 Explanation: The largest element of the given array is 90.
 *************************************************************************************************************/
+
+class Solution {
+    public static int largest(int[] arr) {
+        // code here
+        int n=arr.length;
+        int Max=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>Max){
+                Max=arr[i];
+            }
+        }
+        return Max;
+    }
+}
