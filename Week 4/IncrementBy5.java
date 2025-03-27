9aada9c714462a455020b39e123a6a5e221553a5
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-27 07:50:41 +0530

    Create IncrementBy5.java

---
+++ Week 4/IncrementBy5.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+      
+        // Initialize an integer array with predefined values
+        int[] arr = {1, 12, 14, 20, 30}; 
+        int n = arr.length;  // Get the length of the array
+
+        // Iterate through the array and add 5 to each element
+        for (int i = 0; i < n; i++) { 
+            arr[i] = arr[i] + 5; 
+        }
+
+        // Iterate through the modified array and print each element
+        for (int j = 0; j < n; j++) { 
+            System.out.print(arr[j] + " "); 
+        }
+    }
+}
