5781b9d0feaf7ae9e2ab513c1bb3dfc97dcde280
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-28 12:17:24 +0530

    Update Pattern5.java

--- Week 2/Pattern5.java
+++ Week 2/Pattern5.java
@@ -5,6 +5,8 @@ Pattern 5
 1 2  
 1  
 *************************************************************************************************************/
+
+// Solution 1
 public class Pattern5 {
     public static void main(String[] args) {
         int N = 4;    // Define the number of rows for the pattern
@@ -16,6 +18,19 @@ public class Pattern5 {
                 System.out.print(j + " ");    // Print the number with a space
             }
             System.out.println();    // Move to the next line after printing a row
+        }
+    }
+}
+
+// Solution 2
+public class Main{
+    public static void main(String[] args){
+        int n=4;
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
         }
     }
 }
