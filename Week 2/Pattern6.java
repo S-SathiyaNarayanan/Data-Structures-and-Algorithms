729f0d8fa59b4967c9b8a0a015775072767298d5
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-28 15:04:03 +0530

    Update Pattern6.java

--- Week 2/Pattern6.java
+++ Week 2/Pattern6.java
@@ -21,3 +21,16 @@ public class Pattern6 {
         }
     }
 }
+
+// Solution 2
+public class Pattern6{
+    public static void main(String[] args){
+        int n=4;
+        for(int i=n;i>=1;i--){
+            for(int j=i;j>=1;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
