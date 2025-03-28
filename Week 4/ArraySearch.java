1b85fff3c57e174b071f62566722e510a7d930a0
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-28 07:00:41 +0530

    Update ArraySearch.java

--- Week 4/ArraySearch.java
+++ Week 4/ArraySearch.java
@@ -1,1 +1,9 @@
-
+/*************************************************************************************************************
+🪙GeeksforGeeks - Array Search
+Given an array, arr of n integers, and an integer element x, find whether element x is present in the array. 
+Return the index of the first occurrence of x in the array, or -1 if it doesn't exist.
+Input: arr[] = [1, 2, 3, 4], x = 3
+Output: 2
+Explanation: There is one test case with array as [1, 2, 3 4] and element to be searched as 3. Since 3 is 
+present at index 2, the output is 2.
+*************************************************************************************************************/
