8264af235945eccc8669141b725cef7ed11d2a16
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-29 04:33:15 +0530

    Create CornerDigitsSum.java

---
+++ Week 2/CornerDigitsSum.java
@@ -0,0 +1,7 @@
+/*************************************************************************************************************
+🪙GeeksforGeeks - Corner Digits Sum
+Given a number N, find the sum of first and last digit of N.
+Input: N = 12345
+Output: 6
+Explanation: 1st and last digits are 1 and 5.
+*************************************************************************************************************/
