347e52c929f28614f1fd36b6172961cfc72d3237
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-29 08:42:36 +0530

    Update Pattern12.java

--- Week 2/Pattern12.java
+++ Week 2/Pattern12.java
@@ -13,7 +13,6 @@ Expected output:
 *************************************************************************************************************/
 
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
