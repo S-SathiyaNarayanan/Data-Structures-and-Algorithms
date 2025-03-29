3f8396d31c17087890de179daee43c0c10ee637d
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-03-29 08:47:33 +0530

    Update Pattern14.java

--- Week 2/Pattern14.java
+++ Week 2/Pattern14.java
@@ -1,1 +1,30 @@
+/*************************************************************************************************************
+Pattern 14
+Input: 
+n = 5
+Expected output:
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+*************************************************************************************************************/
+
+import java.util.Scanner; 
+public class Pattern14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);        
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();        // Read input from user
+
+        // Outer loop controls the number of rows
+        for (int i = 1; i <= n; i++) {
+            // Inner loop prints the number 'i', 'i' times
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " "); // Print 'i'
+            }
+            System.out.println(); // Move to the next line
+        }
+    }
+}
 
