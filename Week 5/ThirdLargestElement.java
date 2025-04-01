6bb1cf34a2d1981828c54dc23cccd9e9a8a4a897
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-01 20:54:07 +0530

    Create ThirdLargestElement.java

---
+++ Week 5/ThirdLargestElement.java
@@ -0,0 +1,8 @@
+/*************************************************************************************************************
+🪙GeeksforGeeks - Third largest element
+Given an array, arr of positive integers. Find the third largest element in it. Return -1 if the third largest
+element is not found.
+Input: arr[] = [2, 4, 1, 3, 5]
+Output: 3
+Explanation: The third largest element in the array [2, 4, 1, 3, 5] is 3.
+*************************************************************************************************************/
