8ff64b5c0e4f01e792a27fb24f2fabbe1c605b49
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-04 15:17:44 +0530

    Update ReverseTheNumber.java

--- Week 3/ReverseTheNumber.java
+++ Week 3/ReverseTheNumber.java
@@ -1,7 +1,23 @@
 /*************************************************************************************************************
 Write a program that gets n as input and print the reverse of the number
 Input:
-N = 325345
+n = 325345
 Output:
 543523
 *************************************************************************************************************/
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n=sc.nextInt();
+        int ReversedNumber=0;
+        while(n>0){
+            int LastDigit=n%10;    // Get the last digit
+            ReversedNumber=ReversedNumber*10+LastDigit;    // Append it to reversed number
+            n=n/10;    // Remove the last digit
+        }
+        // Value of n becomes 0
+        System.out.println("The Reversed value is "+ReversedNumber);
+    }
+}
