9c63b0f25be5f7403111aad1178c2f321c03070a
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-07 22:03:59 +0530

    Update BasicsOfArray.java

--- Week 3/BasicsOfArray.java
+++ Week 3/BasicsOfArray.java
@@ -1,1 +1,20 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+        // Declare and initialize an array of integers
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Print all elements using a for loop
+        System.out.println("Elements of the array:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+
+        // Calculate and print the sum of all elements
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        System.out.println("Sum of all elements: " + sum);
+    }
+}
 
