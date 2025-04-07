157a7b2fa7dc48e9923da4c8086d9720439460a5
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-07 23:49:31 +0530

    Update DynamicAndStaticArray.java

--- Week 3/DynamicAndStaticArray.java
+++ Week 3/DynamicAndStaticArray.java
@@ -1,36 +1,37 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class StaticDynamicArrayDemo {
+public class StaticDynamicArrayWithSize {
     public static void main(String[] args) {
         // ===== STATIC ARRAY =====
-        int[] staticArray = {10, 20, 30, 40, 50}; // size is fixed
+        int[] staticArray = {10, 20, 30, 40, 50}; // fixed size
+        int staticSize = staticArray.length; // size of static array
 
         System.out.println("Static Array Elements:");
-        for (int i = 0; i < staticArray.length; i++) {
+        for (int i = 0; i < staticSize; i++) {
             System.out.println(staticArray[i]);
         }
-        System.out.println("Size of static array: " + staticArray.length);
+        System.out.println("Size of static array: " + staticSize);
 
         // ===== DYNAMIC ARRAY =====
         ArrayList<Integer> dynamicArray = new ArrayList<>();
 
-        // Add elements dynamically using Scanner
         Scanner scanner = new Scanner(System.in);
         System.out.println("\nEnter 5 integers to add to the dynamic array:");
         for (int i = 0; i < 5; i++) {
             System.out.print("Enter number " + (i + 1) + ": ");
             int number = scanner.nextInt();
-            dynamicArray.add(number); // dynamically adds element
+            dynamicArray.add(number); // dynamic insert
         }
 
-        // Display dynamic array
+        int dynamicSize = dynamicArray.size(); // size of dynamic array
+
         System.out.println("\nDynamic Array Elements:");
-        for (int i = 0; i < dynamicArray.size(); i++) {
+        for (int i = 0; i < dynamicSize; i++) {
             System.out.println(dynamicArray.get(i));
         }
-        System.out.println("Size of dynamic array: " + dynamicArray.size());
-        
+        System.out.println("Size of dynamic array: " + dynamicSize);
+
         scanner.close();
     }
 }
