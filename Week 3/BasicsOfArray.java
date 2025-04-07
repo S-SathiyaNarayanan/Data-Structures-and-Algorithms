efac968f22fd482f0901fa2acf0224d84496ae62
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-07 22:04:31 +0530

    Update BasicsOfArray.java

--- Week 3/BasicsOfArray.java
+++ Week 3/BasicsOfArray.java
@@ -10,11 +10,16 @@ public class ArrayDeclarationDemo {
         // 2. Declaration and initialization in one line using 'new'
         int[] array2 = new int[] {20, 25, 30};
 
-        // 3. Declaration and initialization using shorthand (most common)
+        // 3. Declaration and initialization using shorthand
         int[] array3 = {35, 40, 45};
 
+        // Print sizes using .length
+        System.out.println("Size of array1: " + array1.length);
+        System.out.println("Size of array2: " + array2.length);
+        System.out.println("Size of array3: " + array3.length);
+
         // Print all arrays
-        System.out.println("Array1 elements:");
+        System.out.println("\nArray1 elements:");
         for (int i = 0; i < array1.length; i++) {
             System.out.println(array1[i]);
         }
