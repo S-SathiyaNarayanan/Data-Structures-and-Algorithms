08a1429b60e0a15da9bd673bfecd8cc99f8a2cf6
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-08 22:41:31 +0530

    Update TypeCasting.java

--- Week 1/TypeCasting.java
+++ Week 1/TypeCasting.java
@@ -29,7 +29,7 @@ public class TypeCasting {
         int charToInt = charValue;    // Automatic char->int (ASCII value)
         System.out.println("char to int: " + charToInt);
 
-        // ----------------------------
+    
         // Narrowing Casting (Manual)
         // ----------------------------
         // Rules:
