da59f51bfaa44b8b3c1a454f5206eb95d3141c74
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-11 13:04:35 +0530

    Update Pattern13.java

--- Week 2/Pattern13.java
+++ Week 2/Pattern13.java
@@ -9,7 +9,24 @@ Expected output:
 7 8 9 10
 11 12 13 14 15
 *************************************************************************************************************/
+// Solution 1
+import java.util.Scanner;
+public class Pattern13 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n=sc.nextInt();
+        int count=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(++count+" ");
+            }
+            System.out.println();
+        }
+    }
+}
 
+// Solution 2
 import java.util.Scanner;        // Import Scanner class for user input
 public class Pattern13 {
     public static void main(String[] args) {
