7c2c933cb99fcbc704e7e1ca0ac340bfd6cc7716
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-15 16:30:24 +0530

    Update PrimeNumber.java

--- Week 3/PrimeNumber.java
+++ Week 3/PrimeNumber.java
@@ -1,1 +1,10 @@
-
+/*************************************************************************************************************
+🪙GeeksforGeeks - Prime Number
+Given a number n, determine whether it is a prime number or not. 
+Input: n = 7
+Output: true
+Explanation: 7 has exactly two divisors: 1 and 7, making it a prime number.
+Input: n = 1
+Output: false
+Explanation: 1 has only one divisor (1 itself), which is not sufficient for it to be considered prime.
+*************************************************************************************************************/
