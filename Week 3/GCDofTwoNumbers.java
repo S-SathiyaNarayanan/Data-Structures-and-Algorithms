4f95c16986aafca81fd7632382e241712c5f5df7
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-15 18:23:49 +0530

    Update GCDofTwoNumbers.java

--- Week 3/GCDofTwoNumbers.java
+++ Week 3/GCDofTwoNumbers.java
@@ -5,4 +5,7 @@ Note: Don't use the inbuilt gcd function
 Input: a = 3, b = 6
 Output: 3
 Explanation: GCD of 3 and 6 is 3
+Input: a = 1, b = 1
+Output: 1
+Explanation: GCD of 1 and 1 is 1
 *************************************************************************************************************/
