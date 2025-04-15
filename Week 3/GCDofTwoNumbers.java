5e2ad7a9b2d6666e978d8b398f8690e34ce47797
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-15 18:23:34 +0530

    Update GCDofTwoNumbers.java

--- Week 3/GCDofTwoNumbers.java
+++ Week 3/GCDofTwoNumbers.java
@@ -2,10 +2,7 @@
 🪙GeeksforGeeks - GCD of two numbers
 Given two positive integers a and b, find GCD of a and b.
 Note: Don't use the inbuilt gcd function
-Input: n = 7
-Output: true
-Explanation: 7 has exactly two divisors: 1 and 7, making it a prime number.
-Input: n = 1
-Output: false
-Explanation: 1 has only one divisor (1 itself), which is not sufficient for it to be considered prime.
+Input: a = 3, b = 6
+Output: 3
+Explanation: GCD of 3 and 6 is 3
 *************************************************************************************************************/
