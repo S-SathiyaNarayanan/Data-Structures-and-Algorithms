251b4953daca74fe3248d8e214d983a7beb8bc37
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-15 17:47:55 +0530

    Update SwapTwoVariablesValues.java

--- Week 3/SwapTwoVariablesValues.java
+++ Week 3/SwapTwoVariablesValues.java
@@ -1,1 +1,19 @@
+// Solution 1 
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of a: ");
+        int a = sc.nextInt();  // Asking user to enter the value of 'a'
+        System.out.print("Enter the value of b: ");
+        int b = sc.nextInt();  // Asking user to enter the value of 'b'
+        // Swapping logic begins
+        int temp = a;    // Step 1: Store the value of 'a' in a temporary variable 'temp'
+        a = b;           // Step 2: Assign the value of 'b' to 'a'        
+        b = temp;        // Step 3: Assign the value stored in 'temp' (original 'a') to 'b'
+        System.out.println("\n--Values after Swapping--");  
+        System.out.println("The value of a is " + a);
+        System.out.println("The value of b is " + b);
+    }
+}
 
