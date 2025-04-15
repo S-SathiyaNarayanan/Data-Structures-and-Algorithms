8bf0795d273599c3292cdc24c38d313ae43192bf
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-15 21:10:13 +0530

    Update SumOfNaturalNumbers.java

--- Week 3/SumOfNaturalNumbers.java
+++ Week 3/SumOfNaturalNumbers.java
@@ -1,1 +1,8 @@
-
+/*************************************************************************************************************
+🪙GeeksforGeeks - Sum of Natural Numbers
+You are given an integer n. Your task is to reverse the digits, ensuring that the reversed number has no 
+leading zeroes.
+Input : n = 200
+Output: 2
+Explanation: By reversing the digits of number, number will change into 2.
+*************************************************************************************************************/
