4aaf080b4dbace64dc2b4479f9383078f406fb84
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-15 19:32:18 +0530

    Update MiddleOfThree.java

--- Week 3/MiddleOfThree.java
+++ Week 3/MiddleOfThree.java
@@ -1,1 +1,8 @@
-
+/*************************************************************************************************************
+🪙GeeksforGeeks - Middle of Three
+Given three distinct numbers a, b and c. Find the number with a value in the middle (Try to do it with minimum
+comparisons)
+Input: a = 978, b = 518, c = 300
+Output: 518
+Explanation: Since 518>300 and 518<978, so 518 is the middle element.
+*************************************************************************************************************/
