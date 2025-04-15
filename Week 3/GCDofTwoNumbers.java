999cb426c690b18798cb8cef762aebfafbf2557f
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-15 18:37:13 +0530

    Update GCDofTwoNumbers.java

--- Week 3/GCDofTwoNumbers.java
+++ Week 3/GCDofTwoNumbers.java
@@ -10,15 +10,17 @@ Output: 1
 Explanation: GCD of 1 and 1 is 1
 *************************************************************************************************************/
 
-// Solution 1 - Euclidean algorithm
+// Solution 1 - Euclidean algorithm  
+// Euclidean algorithm principle is GCD(a, b) = GCD(b, a % b)
 class Solution {
     public static int gcd(int a, int b) {
         // Loop runs until b becomes 0
         while(b!=0){
-            int temp = b;    // Store the current value of b in a temporary variable
-            b = a % b;    // Update b to the remainder of a divided by b
-            a = temp;
+            int temp = b;  // Store the current value of b in a temporary variable
+            b = a % b;     // Update b to the remainder of a divided by b  
+            a = temp;      // Assign the previous value of b (stored in temp) to a
         }
+        // When b becomes 0, a contains the GCD
         return a;
     }
 }
