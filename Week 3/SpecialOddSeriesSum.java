2057d29a271c697479fe14f48e4d01a5fe79b9f2
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-16 19:25:21 +0530

    Update SpecialOddSeriesSum.java

--- Week 3/SpecialOddSeriesSum.java
+++ Week 3/SpecialOddSeriesSum.java
@@ -1,1 +1,12 @@
-
+/*************************************************************************************************************
+🪙GeeksforGeeks - Special Odd Series Sum
+Given the value of n, we need to find the sum of the series where i-th term is sum of first i odd natural 
+numbers
+NOTE: Sum of the series 1 + (1+3) + (1+3+5) + (1+3+5+7) + …… + (1+3+5+7+…+(2n-1))
+Input: n = 4 
+Output: 10
+Explanation: The 4th term of the Series is 10.
+Input: n = 3 
+Output: 6
+Explanation: The 3rd term of the Series is 6.
+*************************************************************************************************************/
