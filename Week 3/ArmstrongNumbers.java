ec2e60098991c49a58a960efcfd7a0e137113073
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-16 18:46:28 +0530

    Update ArmstrongNumbers.java

--- Week 3/ArmstrongNumbers.java
+++ Week 3/ArmstrongNumbers.java
@@ -2,11 +2,11 @@
 🪙GeeksforGeeks - Armstrong Numbers
 You are given a 3 digit number n, Find whether it is an Armstrong number or not
 Note: An Armstrong number of three digits is a number such that the sum of the cubes of its digits is equal to
-the number itself. 371 is an Armstrong number since 33 + 73 + 13 = 371 
+the number itself. 371 is an Armstrong number since 3^3 + 7^3 + 1^3 = 371 
 Input: n = 153
 Output: true
-Explanation: 153 is an Armstrong number since 13 + 53 + 33 = 153 
+Explanation: 153 is an Armstrong number since 1^3 + 5^3 + 3^3 = 153 
 Input: n = 372
 Output: false
-Explanation: 372 is not an Armstrong number since 33 + 73 + 23 = 378 
+Explanation: 372 is not an Armstrong number since 3^3 + 7^3 + 2^3 = 378 
 *************************************************************************************************************/
