4d383920850692ed49bd9d114effbf7ca944b745
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-16 19:12:40 +0530

    Update SpecialSeriesSum.java

--- Week 3/SpecialSeriesSum.java
+++ Week 3/SpecialSeriesSum.java
@@ -1,1 +1,11 @@
-
+/*************************************************************************************************************
+🪙GeeksforGeeks - Special Series Sum
+Given two positive integers a and b, find GCD of a and b.
+Note: Don't use the inbuilt gcd function
+Input: a = 3, b = 6
+Output: 3
+Explanation: GCD of 3 and 6 is 3
+Input: a = 1, b = 1
+Output: 1
+Explanation: GCD of 1 and 1 is 1
+*************************************************************************************************************/
