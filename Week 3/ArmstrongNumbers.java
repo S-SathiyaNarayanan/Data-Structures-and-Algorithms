296b261828cc51389f623e7ea9be8966df2ff38f
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-16 18:42:23 +0530

    Update ArmstrongNumbers.java

--- Week 3/ArmstrongNumbers.java
+++ Week 3/ArmstrongNumbers.java
@@ -1,10 +1,11 @@
 /*************************************************************************************************************
 🪙GeeksforGeeks - Armstrong Numbers
 You are given a 3 digit number n, Find whether it is an Armstrong number or not
-Note: Don't use the inbuilt gcd function
-Input: a = 3, b = 6
-Output: 3
-Explanation: GCD of 3 and 6 is 3
+Note: An Armstrong number of three digits is a number such that the sum of the cubes of its digits is equal to
+the number itself. 371 is an Armstrong number since 33 + 73 + 13 = 371 
+Input: n = 153
+Output: true
+Explanation: 153 is an Armstrong number since 13 + 53 + 33 = 153. 
 Input: a = 1, b = 1
 Output: 1
 Explanation: GCD of 1 and 1 is 1
