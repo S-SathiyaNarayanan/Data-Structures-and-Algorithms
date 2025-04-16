2cb34d5afaa8cb28518ec175e209066c23be3b62
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-16 18:42:50 +0530

    Update ArmstrongNumbers.java

--- Week 3/ArmstrongNumbers.java
+++ Week 3/ArmstrongNumbers.java
@@ -5,8 +5,8 @@ Note: An Armstrong number of three digits is a number such that the sum of the c
 the number itself. 371 is an Armstrong number since 33 + 73 + 13 = 371 
 Input: n = 153
 Output: true
-Explanation: 153 is an Armstrong number since 13 + 53 + 33 = 153. 
-Input: a = 1, b = 1
-Output: 1
-Explanation: GCD of 1 and 1 is 1
+Explanation: 153 is an Armstrong number since 13 + 53 + 33 = 153 
+Input: n = 372
+Output: false
+Explanation: 372 is not an Armstrong number since 33 + 73 + 23 = 378 
 *************************************************************************************************************/
