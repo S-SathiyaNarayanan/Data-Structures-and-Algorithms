4aa4ab48e0153a46f22abde908bb1f682f1c7750
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-16 19:16:18 +0530

    Update SpecialSeriesSum.java

--- Week 3/SpecialSeriesSum.java
+++ Week 3/SpecialSeriesSum.java
@@ -1,11 +1,11 @@
 /*************************************************************************************************************
 🪙GeeksforGeeks - Special Series Sum
-Given two positive integers a and b, find GCD of a and b.
-Note: Don't use the inbuilt gcd function
-Input: a = 3, b = 6
-Output: 3
-Explanation: GCD of 3 and 6 is 3
-Input: a = 1, b = 1
-Output: 1
-Explanation: GCD of 1 and 1 is 1
+Given the value of n, we need to find the sum of the series where i-th term is sum of first i natural numbers
+Note: Sum of the series 1 + (1+2) + (1+2+3) + (1+2+3+4) + …… + (1+2+3+4+…+n)
+Input: n = 5
+Output: 35 
+Explanation: 1 + (1+2) + (1+2+3).. = 35. Hence sum of the series is 35
+Input: n = 10
+Output: 220
+Explanation: 1 + (1+2) + (1+2+3) + (1+2+3+4) + (1+2+3+4+5)... = 220. Hence sum of the series is 220
 *************************************************************************************************************/
