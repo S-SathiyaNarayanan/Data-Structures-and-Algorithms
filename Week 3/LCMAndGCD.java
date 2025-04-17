59fa3d3d179b443b7d544e437b0dda13f8367392
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-17 22:27:46 +0530

    Update LCMAndGCD.java

--- Week 3/LCMAndGCD.java
+++ Week 3/LCMAndGCD.java
@@ -1,3 +1,15 @@
+/*************************************************************************************************************
+🪙GeeksforGeeks - LCM And GCD
+Given two positive integers a and b, find GCD of a and b.
+Note: Don't use the inbuilt gcd function
+Input: a = 3, b = 6
+Output: 3
+Explanation: GCD of 3 and 6 is 3
+Input: a = 1, b = 1
+Output: 1
+Explanation: GCD of 1 and 1 is 1
+*************************************************************************************************************/
+
 class Solution {
     public static int[] lcmAndGcd(int a, int b) {
         // Store original values of a and b for LCM calculation
