f54e1e8142ebb759886c2452fa7cba07ced966bc
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-17 12:36:26 +0530

    Update SquareRoot.java

--- Week 3/SquareRoot.java
+++ Week 3/SquareRoot.java
@@ -1,1 +1,8 @@
-
+/*************************************************************************************************************
+🪙GeeksforGeeks - Square Root
+Given a positive integer n find the square root of n. If n is not a perfect square then return the floor value
+Note: Floor value of any number is the greatest Integer which is less than or equal to that number
+Input: n = 4
+Output: 2
+Explanation: Since, 4 is a perfect square, so its square root is 2
+*************************************************************************************************************/
