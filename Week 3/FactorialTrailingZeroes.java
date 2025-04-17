757e628074762e5dcc1726893cc3924a0b9c2c12
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-17 19:17:43 +0530

    Update FactorialTrailingZeroes.java

--- Week 3/FactorialTrailingZeroes.java
+++ Week 3/FactorialTrailingZeroes.java
@@ -1,1 +1,12 @@
-
+/*************************************************************************************************************
+💎LeetCode 172 - Factorial Trailing Zeroes
+🪙GeeksforGeeks - Trailing zeroes in factorial
+Given an integer n, return the number of trailing zeroes in n!
+Note: That n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1
+Input: n = 3
+Output: 0
+Explanation: 3! = 6, no trailing zero
+Input: n = 5
+Output: 1
+Explanation: 5! = 120, one trailing zero
+*************************************************************************************************************/
