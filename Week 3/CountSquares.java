7f3b300af34ded2f9643bfbc17d84f1f1e959ca7
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-18 04:39:06 +0530

    Update CountSquares.java

--- Week 3/CountSquares.java
+++ Week 3/CountSquares.java
@@ -9,3 +9,14 @@ Input: n = 3
 Output: 1
 Explanation: 1 is the only Perfect Square less than 3. So, the Output is 1
 *************************************************************************************************************/
+
+class Solution {
+    static int countSquares(int n) {
+        // code here
+        int count=0;
+        for(int i=1;i*i<n;i++){
+            count++;
+        }
+        return count;
+    }
+}
