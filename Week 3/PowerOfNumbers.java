c309113ddde623f1808c31d08d4081947e1edc06
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-17 12:05:54 +0530

    Update PowerOfNumbers.java

--- Week 3/PowerOfNumbers.java
+++ Week 3/PowerOfNumbers.java
@@ -1,1 +1,8 @@
-
+/*************************************************************************************************************
+🪙GeeksforGeeks - Power Of Numbers
+Given a number n, find the value of n raised to the power of its own reverse
+Note: The result will always fit into a 32-bit signed integer.
+Input: n = 2
+Output: 4
+Explanation: The reverse of 2 is 2, and 2^2 = 4.
+*************************************************************************************************************/
