73d7a7b440f7d6a9427f1ce034417011560264e8
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-17 22:04:31 +0530

    Update LCMAndGCD.java

--- Week 3/LCMAndGCD.java
+++ Week 3/LCMAndGCD.java
@@ -1,1 +1,19 @@
+class Solution {
+    public static int[] lcmAndGcd(int a, int b) {
+        // Store original values of a and b for LCM calculation
+        int x = a;
+        int y = b;
+        // Use Euclidean algorithm to find GCD
+        while (b != 0) {
+            int temp = b;     // Temporarily store the value of b
+            b = a % b;        // Update b to the remainder of a divided by b
+            a = temp;         // Update a to the previous value of b
+        }
+        int GCD = a;  // After the loop, 'a' holds the GCD
+        // Calculate LCM using the formula: (x * y) / GCD
+        int LCM = (int)((long)x * (long)y) / GCD;
+        // Return result as an array: {LCM, GCD}
+        return new int[]{LCM, GCD};
+    }
+}
 
