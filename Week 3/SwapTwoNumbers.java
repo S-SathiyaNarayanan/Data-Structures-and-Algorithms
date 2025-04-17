3862213b549c64d9d15a5e67dda19eff2e399636
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-17 10:52:12 +0530

    Update SwapTwoNumbers.java

--- Week 3/SwapTwoNumbers.java
+++ Week 3/SwapTwoNumbers.java
@@ -6,3 +6,14 @@ Input: a = 13, b = 9
 Output: 9 13
 Explanation: After swapping it becomes 9 and 13.
 *************************************************************************************************************/
+
+// Solution 1 - 
+class Solution{
+    static List<Integer> get(int a,int b)
+    {
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        return Arrays.asList(a,b);
+    }
+}
