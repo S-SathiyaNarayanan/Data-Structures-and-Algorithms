21260876d126a4209fc84f310c4ca2f283e8448c
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-24 02:49:02 +0530

    Update SumOfFirstNTerms.java

--- Week 3/SumOfFirstNTerms.java
+++ Week 3/SumOfFirstNTerms.java
@@ -1,3 +1,14 @@
+/*************************************************************************************************************
+🪙GeeksforGeeks - Sum of first n terms
+Given an integer n, calculate the sum of series 1^3 + 2^3 + 3^3 + 4^3 + … till n-th term.
+Input: n = 5
+Output: 225
+Explanation: 1^3 + 2^3 + 3^3 + 4^3 + 5^3 = 225
+Input: n = 7
+Output: 784
+Explanation: 1^3 + 2^3 + 3^3 + 4^3 + 5^3 + 6^3 + 7^3 = 784
+*************************************************************************************************************/
+
 // Solution 1 - Using Mathematical Formula
 class Solution {
     int sumOfSeries(int n) {
