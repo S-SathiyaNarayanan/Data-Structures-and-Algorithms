261df01461261c157586cf3b27591e76b6946c06
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-24 01:34:00 +0530

    Update SumOfFirstNTerms.java

--- Week 3/SumOfFirstNTerms.java
+++ Week 3/SumOfFirstNTerms.java
@@ -1,1 +1,9 @@
-
+class Solution {
+    int sumOfSeries(int n) {
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum=sum+(int)Math.pow(i,3);
+        }
+        return sum;
+    }
+}
