de27cbba31326a89751ab7a177088c66f42a9d87
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-24 03:17:18 +0530

    Update SumOfFifthPowersOfN.java

--- Week 3/SumOfFifthPowersOfN.java
+++ Week 3/SumOfFifthPowersOfN.java
@@ -1,1 +1,9 @@
-
+class Solution {
+    long sumOfFifthPowers(long N) {
+        long sum=0;
+        for(int i=1;i<=N;i++){
+            sum=sum+(long)Math.pow(i,5);
+        }
+        return sum;
+    }
+}
