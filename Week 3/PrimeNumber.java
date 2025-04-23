ad98b2673a52cac7ce2e330d059f4c0e98f26203
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-23 17:49:51 +0530

    Update PrimeNumber.java

--- Week 3/PrimeNumber.java
+++ Week 3/PrimeNumber.java
@@ -8,7 +8,7 @@ Input: n = 1
 Output: false
 Explanation: 1 has only one divisor (1 itself), which is not sufficient for it to be considered prime.
 *************************************************************************************************************/
-// Solution 2 - 
+// Solution 2 - Uses Math.sqrt(n)
 class Solution {
     static boolean isPrime(int n) {
         if (n <= 1) return false;        // Check if n is less than or equal to 1 (not prime)
