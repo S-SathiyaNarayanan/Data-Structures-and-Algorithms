e743a715a3453ba10e6a8cea21546676de59cb0a
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-23 22:27:32 +0530

    Update FindN-ThTermOfSeries .java

--- Week 3/FindN-ThTermOfSeries .java
+++ Week 3/FindN-ThTermOfSeries .java
@@ -9,14 +9,14 @@ Output: 6
 Explanation: The 3rd term of the Series is 6.
 *************************************************************************************************************/
 
-// Solution 1
+// Solution 1 - Using the formula for the sum of the first n natural numbers n * (n + 1) / 2
 class Solution {
     static int findNthTerm(int n) {
         return n*(n+1)/2;
     }
 }
 
-// Solution 2
+// Solution 2 - 
 class Solution {
     static int findNthTerm(int n) {
         int sum=0;
