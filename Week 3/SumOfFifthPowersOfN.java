64aceba42ace8cc74c86f7eab18c4a5d61ab04b9
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-25 00:57:32 +0530

    Update SumOfFifthPowersOfN.java

--- Week 3/SumOfFifthPowersOfN.java
+++ Week 3/SumOfFifthPowersOfN.java
@@ -2,8 +2,7 @@
 🪙GeeksforGeeks - Sum of fifth powers of the first n natural numbers
 Given a number N. Find the sum of fifth powers of natural numbers till N i.e. 1^5+2^5+3^5+..+N^5
 Input: N = 3
-Output:
-276
+Output: 276
 Explanation:
 The sum is calculated as 1^5+2^5+3^5 = 1+32+243 = 276.
 *************************************************************************************************************/
