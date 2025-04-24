44550c3e9dec0200930a7a50ec80147fcc549300
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-25 00:44:14 +0530

    Update SumOfFifthPowersOfN.java

--- Week 3/SumOfFifthPowersOfN.java
+++ Week 3/SumOfFifthPowersOfN.java
@@ -1,4 +1,11 @@
 // Solution 1 -
+class Solution {
+    long sumOfFifthPowers(long N) {
+        // code here
+        long sum=((N*N)*((N+1)*(N+1))*((2*N*N)+(2*N)-1))/12;
+        return sum;
+    }
+}
 // Solution 2 - 
 class Solution {
     long sumOfFifthPowers(long N) {
