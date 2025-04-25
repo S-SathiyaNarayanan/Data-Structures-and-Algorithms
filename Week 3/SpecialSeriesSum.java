2c339712d6016073961c8961b649fed455db27fc
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-25 05:44:19 +0530

    Update SpecialSeriesSum.java

--- Week 3/SpecialSeriesSum.java
+++ Week 3/SpecialSeriesSum.java
@@ -18,3 +18,17 @@ class Solution {
 }
 
 // Solution 2 - 
+class Solution {
+    static long sumOfTheSeries(long n){
+        // code here
+        int count=0;
+        long sum=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                count++;
+            }
+            sum=sum+count;
+        }
+        return sum;
+    }
+}
