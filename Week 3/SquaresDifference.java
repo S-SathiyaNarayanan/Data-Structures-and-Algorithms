2e9e0b3c3d4ef10f8452cb38e0317bd6cb52492d
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-25 07:25:50 +0530

    Update SquaresDifference.java

--- Week 3/SquaresDifference.java
+++ Week 3/SquaresDifference.java
@@ -1,1 +1,9 @@
-
+class Solution {
+    static long squaresDiff(int N){
+        long sumOfSquares = (long) N * (N + 1) * (2 * N + 1) / 6;
+        long sumOfFirst = (long) N * (N + 1) / 2;
+        long squareOfSum = sumOfFirst * sumOfFirst;
+        long result = Math.abs(sumOfSquares - squareOfSum);
+        return result;
+    }
+} 
