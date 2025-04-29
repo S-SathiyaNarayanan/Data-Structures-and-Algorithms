dd99182857466a3e7e699592cd4645ae6772392a
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-29 08:16:07 +0530

    Update CountDigits.java

--- Week 3/CountDigits.java
+++ Week 3/CountDigits.java
@@ -1,6 +1,7 @@
 /*************************************************************************************************************
 🪙GeeksforGeeks - Count Digits
-Given a positive integer, N. Find the factorial of N.
+Given a positive integer n, count the number of digits in n that divide n evenly (i.e., without leaving a 
+remainder). Return the total number of such digits.
 Input: N = 5
 Output: 120
 Explanation: 1 x 2 x 3 x 4 x 5 = 120
