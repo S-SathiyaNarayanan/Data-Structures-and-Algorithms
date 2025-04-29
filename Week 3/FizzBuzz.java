441bcadfe1efde4311a8ce02744794c099e498dc
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-30 00:45:01 +0530

    Update FizzBuzz.java

--- Week 3/FizzBuzz.java
+++ Week 3/FizzBuzz.java
@@ -15,15 +15,15 @@ Output: [“1”, “2”, “Fizz”, “4”, “Buzz”, “Fizz”, “7”,
 class Solution {
     public static ArrayList<String> fizzBuzz(int n) {
         ArrayList<String> list = new ArrayList<>();    // Initialize an ArrayList to store the result
-        for(int i = 1; i <= n; i++) {           // Loop from 1 to n (inclusive)
+        for(int i = 1; i <= n; i++) {                  // Loop from 1 to n (inclusive)
             if(i % 3 == 0 && i % 5 == 0) {
-                list.add("FizzBuzz");           // If divisible by both 3 and 5, add "FizzBuzz"
+                list.add("FizzBuzz");                  // If divisible by both 3 and 5, add "FizzBuzz"
             } else if(i % 3 == 0) {
-                list.add("Fizz");               // If divisible only by 3, add "Fizz"
+                list.add("Fizz");                      // If divisible only by 3, add "Fizz"
             } else if(i % 5 == 0) {
-                list.add("Buzz");               // If divisible only by 5, add "Buzz"
+                list.add("Buzz");                      // If divisible only by 5, add "Buzz"
             } else {
-                list.add(String.valueOf(i));    // If none of the above, add the number as a string
+                list.add(String.valueOf(i));           // If none of the above, add the number as a string
             }
         }
         return list;    // Return the completed list
