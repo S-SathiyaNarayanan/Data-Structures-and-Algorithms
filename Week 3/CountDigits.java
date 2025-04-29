55c3e92d2bbeb37873e15085117966083faee087
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-29 08:16:21 +0530

    Update CountDigits.java

--- Week 3/CountDigits.java
+++ Week 3/CountDigits.java
@@ -2,7 +2,7 @@
 🪙GeeksforGeeks - Count Digits
 Given a positive integer n, count the number of digits in n that divide n evenly (i.e., without leaving a 
 remainder). Return the total number of such digits.
-Input: N = 5
-Output: 120
-Explanation: 1 x 2 x 3 x 4 x 5 = 120
+Input: n = 12
+Output: 2
+Explanation: 1, 2 when both divide 12 leaves remainder 0
 *************************************************************************************************************/
