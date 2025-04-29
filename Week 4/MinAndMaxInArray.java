818b9b33f873a51867195671c451568b433ddd1b
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-30 04:44:58 +0530

    Update MinAndMaxInArray.java

--- Week 4/MinAndMaxInArray.java
+++ Week 4/MinAndMaxInArray.java
@@ -6,25 +6,7 @@ Output: 1 10000
 Explanation: minimum and maximum elements of array are 1 and 10000.
 *************************************************************************************************************/
 // Solution 1
-class Solution {
-    public Pair<Integer, Integer> getMinMax(int[] arr) {
-        // Code Here
-        int n=arr.length;
-        int Max=arr[0];
-        int Min=arr[0];
-        for(int i=0;i<n;i++){
-            if(arr[i]>Max){
-                Max=arr[i];
-            }
-        }
-        for(int j=0;j<n;j++){
-            if(arr[j]<Min){
-                Min=arr[j];
-            }
-        }
-        return new Pair<>(Min,Max);
-    }
-}
+
 
 // Solution 2
 class Solution {
