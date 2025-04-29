e8012f11c1ea4b8125ecd11bdb58ece6104ec3d6
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-29 06:34:36 +0530

    Update OddorEven.java

--- Week 3/OddorEven.java
+++ Week 3/OddorEven.java
@@ -1,6 +1,7 @@
 /*************************************************************************************************************
 🪙GeeksforGeeks - Odd or Even
-Given a positive integer, N. Find the factorial of N.
+Given a positive integer n, determine whether it is odd or even. Return true if the number is even and false 
+if the number is odd
 Input: N = 5
 Output: 120
 Explanation: 1 x 2 x 3 x 4 x 5 = 120
