4c90247b6b38bea341bc72d96a01248bf94fb866
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-29 06:34:53 +0530

    Update OddorEven.java

--- Week 3/OddorEven.java
+++ Week 3/OddorEven.java
@@ -2,7 +2,7 @@
 🪙GeeksforGeeks - Odd or Even
 Given a positive integer n, determine whether it is odd or even. Return true if the number is even and false 
 if the number is odd
-Input: N = 5
-Output: 120
-Explanation: 1 x 2 x 3 x 4 x 5 = 120
+Input: n = 15
+Output: false
+Explanation: The number is not divisible by 2
 *************************************************************************************************************/
