9ce4591b6dc0fb0999225bde8336832e74a2be04
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-29 08:23:23 +0530

    Update CountDigits.java

--- Week 3/CountDigits.java
+++ Week 3/CountDigits.java
@@ -7,4 +7,7 @@ be checked individually. If a digit is 0, it should be ignored because division 
 Input: n = 12
 Output: 2
 Explanation: 1, 2 when both divide 12 leaves remainder 0
+Input: n = 2446
+Output: 1
+Explanation: Here among 2, 4, 6 only 2 divides 2446 evenly while 4 and 6 do not
 *************************************************************************************************************/
