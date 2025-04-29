d1efcea2d612b20768aa5ce83a8f4975c010e3ff
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-30 00:34:42 +0530

    Update FizzBuzz.java

--- Week 3/FizzBuzz.java
+++ Week 3/FizzBuzz.java
@@ -11,3 +11,22 @@ Input: n = 20
 Output: [“1”, “2”, “Fizz”, “4”, “Buzz”, “Fizz”, “7”, “8”, “Fizz”, “Buzz”, “11”, “Fizz”, “13”, “14”, “FizzBuzz”,
 “16”, “17”, “Fizz”, “19”, “Buzz”]
 *************************************************************************************************************/
+
+class Solution {
+    public static ArrayList<String> fizzBuzz(int n) {
+        // code here
+        ArrayList<String> list = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0){
+                list.add("FizzBuzz");
+            }else if(i%3==0){
+                list.add("Fizz");
+            }else if(i%5==0){
+                list.add("Buzz");
+            }else{
+                list.add(String.valueOf(i));
+            }
+        }
+        return list;
+    }
+}
