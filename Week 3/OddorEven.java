18b22cd6f0455a646d252819de76ac59e2a37612
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-29 06:35:16 +0530

    Update OddorEven.java

--- Week 3/OddorEven.java
+++ Week 3/OddorEven.java
@@ -2,6 +2,9 @@
 🪙GeeksforGeeks - Odd or Even
 Given a positive integer n, determine whether it is odd or even. Return true if the number is even and false 
 if the number is odd
+Input: n = 44
+Output: true
+Explanation: The number is divisible by 2
 Input: n = 15
 Output: false
 Explanation: The number is not divisible by 2
