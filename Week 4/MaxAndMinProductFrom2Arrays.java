8cea257b632839f8ad01fbac9f50795e0ec6b348
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-30 17:58:27 +0530

    Update MaxAndMinProductFrom2Arrays.java

--- Week 4/MaxAndMinProductFrom2Arrays.java
+++ Week 4/MaxAndMinProductFrom2Arrays.java
@@ -1,1 +1,8 @@
-
+/*************************************************************************************************************
+🪙GeeksforGeeks - Find element at a given Index
+Given an array arr of integers and an index key(0-based index). Your task is to return the element present at 
+the index key in the array.
+Input: key = 4 , arr = [10, 20, 30, 40, 50, 60, 70]
+Output: 50
+Explanation: The value of the arr[4] is 50 .
+*************************************************************************************************************/
