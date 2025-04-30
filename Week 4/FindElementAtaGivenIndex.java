944da4bc6082ecfa52ba9b86317bd11c90fbc397
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-30 17:01:00 +0530

    Update FindElementAtaGivenIndex.java

--- Week 4/FindElementAtaGivenIndex.java
+++ Week 4/FindElementAtaGivenIndex.java
@@ -1,5 +1,5 @@
 /*************************************************************************************************************
-🪙GeeksforGeeks - Array Search
+🪙GeeksforGeeks - Find element at a given Index
 Given an array, arr of n integers, and an integer element x, find whether element x is present in the array. 
 Return the index of the first occurrence of x in the array, or -1 if it doesn't exist.
 Input: arr[] = [1, 2, 3, 4], x = 3
