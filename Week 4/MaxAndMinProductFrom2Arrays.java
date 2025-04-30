40a33bcf59d45c13959e408c9c195329d90fb413
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-30 18:05:02 +0530

    Update MaxAndMinProductFrom2Arrays.java

--- Week 4/MaxAndMinProductFrom2Arrays.java
+++ Week 4/MaxAndMinProductFrom2Arrays.java
@@ -2,7 +2,7 @@
 🪙GeeksforGeeks - Max and Min Product from 2 Arrays
 Given two arrays of arr1 and arr2, the task is to calculate the product of the maximum element of the first 
 array arr1, and minimum element of the second array arr2.
-Input: key = 4 , arr = [10, 20, 30, 40, 50, 60, 70]
-Output: 50
-Explanation: The value of the arr[4] is 50 .
+Input : arr1 = [5, 7, 9, 3, 6, 2]  arr2 = [1, 2, 6, 1, 9]
+Output : 9
+Explanation: The max in arr1 is 9. The min element in arr2 is 1. The product is 9*1 = 9.
 *************************************************************************************************************/
