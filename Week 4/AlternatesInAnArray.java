2c98fc7406a67c894da2ed06e49a8354888d9c58
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-30 15:44:37 +0530

    Update AlternatesInAnArray.java

--- Week 4/AlternatesInAnArray.java
+++ Week 4/AlternatesInAnArray.java
@@ -12,17 +12,12 @@ Skip fourth element: 4
 *************************************************************************************************************/
 
 class Solution {
-    public ArrayList<Integer> getAlternates(int arr[]) {
-        
+    public ArrayList<Integer> getAlternates(int arr[]) {   
         ArrayList<Integer> arr2 = new ArrayList<>();    // Create a new ArrayList to store the alternate elements
-        
-        for(int i = 0; i < arr.length; i = i + 2) {    // Iterate through the array, incrementing by 2 to get alternate elements
-            
-            arr2.add(arr[i]);// Add the current element (at even index) to the ArrayList
+        for(int i = 0; i < arr.length; i = i + 2) {     // Iterate through the array, incrementing by 2 to get alternate elements
+            arr2.add(arr[i]);                           // Add the current element (at even index) to the ArrayList
         }
-
-        
-        return arr2;// Return the ArrayList containing alternate elements
+        return arr2;                                    // Return the ArrayList containing alternate elements
     }
 }
 
