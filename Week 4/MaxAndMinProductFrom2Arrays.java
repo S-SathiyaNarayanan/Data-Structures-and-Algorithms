636f5b3f320ebbb52bb74547aaa45db984fc9e0a
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-30 18:04:27 +0530

    Update MaxAndMinProductFrom2Arrays.java

--- Week 4/MaxAndMinProductFrom2Arrays.java
+++ Week 4/MaxAndMinProductFrom2Arrays.java
@@ -1,7 +1,7 @@
 /*************************************************************************************************************
 🪙GeeksforGeeks - Max and Min Product from 2 Arrays
-Given an array arr of integers and an index key(0-based index). Your task is to return the element present at 
-the index key in the array.
+Given two arrays of arr1 and arr2, the task is to calculate the product of the maximum element of the first 
+array arr1, and minimum element of the second array arr2.
 Input: key = 4 , arr = [10, 20, 30, 40, 50, 60, 70]
 Output: 50
 Explanation: The value of the arr[4] is 50 .
