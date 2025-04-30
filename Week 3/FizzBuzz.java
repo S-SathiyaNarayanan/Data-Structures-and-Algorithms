6f20521bf5b3e51579675149b6ef24f8ff6a5cff
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-01 01:14:10 +0530

    Update FizzBuzz.java

--- Week 3/FizzBuzz.java
+++ Week 3/FizzBuzz.java
@@ -7,7 +7,7 @@ Fizz Buzz Problem involves that given an integer n, for every integer 0 < i <= n
 "Buzz" if i is divisible by 5,
 "i" as a string, if none of the conditions are true.
 Return an array of strings.
-Input: n = 20
+Input : n = 20
 Output: [“1”, “2”, “Fizz”, “4”, “Buzz”, “Fizz”, “7”, “8”, “Fizz”, “Buzz”, “11”, “Fizz”, “13”, “14”, “FizzBuzz”,
 “16”, “17”, “Fizz”, “19”, “Buzz”]
 *************************************************************************************************************/
