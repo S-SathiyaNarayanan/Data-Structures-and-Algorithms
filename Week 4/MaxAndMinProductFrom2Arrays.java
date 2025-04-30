05566fa40735491e1a97010313dbd0d0bdf366b2
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-04-30 18:13:21 +0530

    Update MaxAndMinProductFrom2Arrays.java

--- Week 4/MaxAndMinProductFrom2Arrays.java
+++ Week 4/MaxAndMinProductFrom2Arrays.java
@@ -12,9 +12,9 @@ class Solution {
         int Max=arr1[0];    // Initialize Max with the first element of arr1
         int Min=arr2[0];    // Initialize Min with the first element of arr2
         for(int i=0;i<arr1.length;i++){     // Loop through arr1 to find the maximum element
-            if(arr1[i]>Max) Max=arr1[i];    
+            if(arr1[i]>Max) Max=arr1[i];    // If the current element is greater than the current Max, update Max
         }for(int j=0;j<arr2.length;j++){    // Loop through arr2 to find the minimum element
-            if(arr2[j]<Min) Min=arr2[j];
+            if(arr2[j]<Min) Min=arr2[j];    // If the current element is less than the current Min, update Min
         }
         return Max*Min;     // Return the product of the maximum from arr1 and minimum from arr2
     }
