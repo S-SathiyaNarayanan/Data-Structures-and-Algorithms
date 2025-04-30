d2b8ed9320431af7bc22cdb8dd36b9ad4c828d33
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-01 01:15:30 +0530

    Update ReverseWithoutZeros.java

--- Week 3/ReverseWithoutZeros.java
+++ Week 3/ReverseWithoutZeros.java
@@ -1,10 +1,10 @@
 /*************************************************************************************************************
-🪙GeeksforGeeks - Reverse digits
-You are given an integer n. Your task is to reverse the digits, ensuring that the reversed number has no 
-leading zeroes.
-Input : n = 200
-Output: 2
-Explanation: By reversing the digits of number, number will change into 2.
+💎LeetCode 7 - Reverse Integer
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go 
+outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+Input : x = 123
+Output: 321
 *************************************************************************************************************/
 
 
