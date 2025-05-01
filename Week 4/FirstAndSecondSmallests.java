902881d2a341a8fb41b5f04de482342bcd958d52
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-01 07:20:48 +0530

    Update FirstAndSecondSmallests.java

--- Week 4/FirstAndSecondSmallests.java
+++ Week 4/FirstAndSecondSmallests.java
@@ -10,3 +10,17 @@ Explanation: 2 and 3 are respectively the smallest and second smallest elements 
 // Solution 1
 
 // Solution 2
+class Solution {
+    public int[] minAnd2ndMin(int arr[]) {
+        int first = Integer.MAX_VALUE, second = Integer.MAX_VALUE;
+        for (int num : arr) {
+            if (num < first) {
+                second = first;
+                first = num;
+            } else if (num < second && num != first) {
+                second = num;
+            }
+        }
+        return second == Integer.MAX_VALUE ? new int[]{-1} : new int[]{first, second};
+    }
+}
