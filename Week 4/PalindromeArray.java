9c6a0d19a59a0cb45b35e767acc72ca691bac3f7
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-02 07:58:09 +0530

    Update PalindromeArray.java

--- Week 4/PalindromeArray.java
+++ Week 4/PalindromeArray.java
@@ -8,3 +8,14 @@ Output: false
 Explanation: Here we can see we have [1, 2, 3, 4, 5] if we reverse it we find [5, 4, 3, 2, 1] which is the not
 same as before. So, the answer false.
 *************************************************************************************************************/
+
+class Solution {
+    public static boolean isPerfect(int[] arr) {
+        // code here
+        int n=arr.length;
+        for(int i=0;i<n/2;i++){
+            if(arr[i]!=arr[n-i-1]) return false; 
+        }
+        return true;
+    }
+}
