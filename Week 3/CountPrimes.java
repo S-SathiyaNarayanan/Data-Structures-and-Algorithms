908ce9214f24eecfd82a0356da7ab115e80267b6
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-03 21:01:34 +0530

    Update CountPrimes.java

--- Week 3/CountPrimes.java
+++ Week 3/CountPrimes.java
@@ -9,7 +9,6 @@ Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7
 class Solution {
     public int countPrimes(int n) {
         int count=0;
-        ArrayList<Integer> PN=new ArrayList<>();
         if(n<2) return count;
         boolean[] arr=new boolean[n+1];
         Arrays.fill(arr,true);
