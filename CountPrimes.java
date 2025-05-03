12b2c79da0f2d7e477a4b13aa7da9a17d222890b
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-03 18:02:01 +0530

    Update CountPrimes.java

--- CountPrimes.java
+++ CountPrimes.java
@@ -1,11 +1,6 @@
 /*************************************************************************************************************
 💎LeetCode 204 - Count Primes
-Fizz Buzz Problem involves that given an integer n, for every integer 0 < i <= n, the task is to output,
-"FizzBuzz" if i is divisible by 3 and 5,
-"Fizz" if i is divisible by 3,
-"Buzz" if i is divisible by 5,
-"i" as a string, if none of the conditions are true.
-Return an array of strings.
+Given an integer n, return the number of prime numbers that are strictly less than n
 Input : n = 20
 Output: [“1”, “2”, “Fizz”, “4”, “Buzz”, “Fizz”, “7”, “8”, “Fizz”, “Buzz”, “11”, “Fizz”, “13”, “14”, “FizzBuzz”,
 “16”, “17”, “Fizz”, “19”, “Buzz”]
