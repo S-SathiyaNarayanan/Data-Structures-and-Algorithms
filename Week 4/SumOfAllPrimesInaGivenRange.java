2e4befd4fff5b550006c101746dd885ad6dab27c
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-03 18:49:47 +0530

    Update SumOfAllPrimesInaGivenRange.java

--- Week 4/SumOfAllPrimesInaGivenRange.java
+++ Week 4/SumOfAllPrimesInaGivenRange.java
@@ -3,6 +3,6 @@ Given a range [L, R]. The task is to find the sum of all the prime numbers in th
 inclusive.
 Input : L = 10, R = 20
 Output : Sum = 60
-Prime numbers between [10, 20] are: 11, 13, 17, 19
+Explanation : Prime numbers between [10, 20] are 11, 13, 17, 19. Therefore sum = 11 + 13 + 17 + 19 = 60
 Note: Use sieve of eratosthenes to solve the problem
 *************************************************************************************************************/
