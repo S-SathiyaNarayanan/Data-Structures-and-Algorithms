eeae09103e75d011b812f3bb9a3cbb70ad7a4378
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-03 20:55:15 +0530

    Update CountPrimesInRange.java

--- Week 4/CountPrimesInRange.java
+++ Week 4/CountPrimesInRange.java
@@ -1,5 +1,5 @@
 /*************************************************************************************************************
-🪙GeeksforGeeks - Sieve of Eratosthenes
+🪙GeeksforGeeks - Count Primes In Range
 Given a positive integer n, calculate and return all prime numbers less than or equal to n using the Sieve of
 Eratosthenes algorithm.
 A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
