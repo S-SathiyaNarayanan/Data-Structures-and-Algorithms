e29766fd7b327470623cd25aee9a160eb66a7a06
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-03 20:56:12 +0530

    Update CountPrimesInRange.java

--- Week 4/CountPrimesInRange.java
+++ Week 4/CountPrimesInRange.java
@@ -1,11 +1,9 @@
 /*************************************************************************************************************
 🪙GeeksforGeeks - Count Primes In Range
-Given a positive integer n, calculate and return all prime numbers less than or equal to n using the Sieve of
-Eratosthenes algorithm.
-A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
-Input : n = 10
-Output: 2 3 5 7
-Explanation: Prime numbers less than equal to 10 are 2 3 5 and 7
+Given two numbers L and R(L<R). Count all the prime numbers in the range [L, R]
+Input : L=1,R=10
+Output: 4
+Explanation : There are 4 primes in this range, which are 2,3,5 and 7
 *************************************************************************************************************/
 
 // Solution 1 - Sieve of Eratosthenes O(nloglogn)
