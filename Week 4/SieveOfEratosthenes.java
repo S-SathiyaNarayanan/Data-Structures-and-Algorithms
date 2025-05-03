6ca5897d7f95a19b22354d2918f7d9022977b211
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-03 11:56:34 +0530

    Update SieveOfEratosthenes.java

--- Week 4/SieveOfEratosthenes.java
+++ Week 4/SieveOfEratosthenes.java
@@ -1,1 +1,25 @@
-
+class Solution {
+    static ArrayList<Integer> sieveOfEratosthenes(int n) {
+        ArrayList<Integer> PN = new ArrayList<>();
+        if (n < 2) {
+            return PN;
+        }
+        boolean[] arr = new boolean[n + 1];
+        Arrays.fill(arr, true);
+        arr[0] = false;
+        arr[1] = false;
+        for (int i = 2; i * i <= n; i++) {
+            if (arr[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    arr[j] = false;
+                }
+            }
+        }
+        for (int i = 2; i <= n; i++) {
+            if (arr[i]) {
+                PN.add(i);
+            }
+        }
+        return PN;
+    }
+}
