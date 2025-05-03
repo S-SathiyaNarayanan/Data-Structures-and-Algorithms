f495219dcdc9d80dee728798a37f6e3b8f0a718e
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-03 20:43:02 +0530

    Update CountPrimesInRange.java

--- Week 4/CountPrimesInRange.java
+++ Week 4/CountPrimesInRange.java
@@ -7,3 +7,26 @@ Input : n = 10
 Output: 2 3 5 7
 Explanation: Prime numbers less than equal to 10 are 2 3 5 and 7
 *************************************************************************************************************/
+class Solution {
+    int countPrimes(int L, int R) {
+        // code here
+        int count=0;
+        int n=R;
+        if(n<2) return count;
+        boolean[] arr=new boolean[n+1];
+        Arrays.fill(arr,true);
+        arr[0]=false;
+        arr[1]=false;
+        for(int i=2;i*i<=n;i++){
+            if(arr[i]){
+                for(int j=i*i;j<=n;j=j+i){
+                    arr[j]=false;
+                }
+            }
+        }
+        for(int i=L;i<=R;i++){
+            if(arr[i]) count++;
+        }
+        return count;
+    }
+}
