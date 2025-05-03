7e46dccfdb10c3efb90cfaec76e51bd3494d02ae
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-03 17:54:34 +0530

    Update AllThePrimeNumbersTillThatNumber.java

--- Week 3/AllThePrimeNumbersTillThatNumber.java
+++ Week 3/AllThePrimeNumbersTillThatNumber.java
@@ -5,3 +5,25 @@ n = 11
 Output:
 2 3 5 7 11
 *************************************************************************************************************/
+class Solution {
+    public int countPrimes(int n) {
+        int count=0;
+        ArrayList<Integer> PN=new ArrayList<>();
+        if(n<2) return count;
+        boolean[] arr=new boolean[n+1];
+        Arrays.fill(arr,true);
+        arr[0]=false;
+        arr[1]=false;
+        for(int i=2;i*i<=n;i++){
+            if(arr[i]){
+                for(int j=i*i;j<=n;j=j+i){
+                    arr[j]=false;
+                }
+            }
+        }
+        for(int i=2;i<n;i++){
+            if(arr[i]) count++;
+        }
+        return count;
+    }
+}
