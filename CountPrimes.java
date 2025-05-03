e2ea1411fd64f8e9fa8181eb2db5f12b765f9a5b
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-03 18:00:59 +0530

    Update CountPrimes.java

--- CountPrimes.java
+++ CountPrimes.java
@@ -1,10 +1,17 @@
 /*************************************************************************************************************
-Write a program that gets n as input and print all the prime numbers till that number.
-Input:
-n = 11
-Output:
-2 3 5 7 11
+💎LeetCode 412 - Fizz Buzz
+🪙GeeksforGeeks - Fizz Buzz
+Fizz Buzz Problem involves that given an integer n, for every integer 0 < i <= n, the task is to output,
+"FizzBuzz" if i is divisible by 3 and 5,
+"Fizz" if i is divisible by 3,
+"Buzz" if i is divisible by 5,
+"i" as a string, if none of the conditions are true.
+Return an array of strings.
+Input : n = 20
+Output: [“1”, “2”, “Fizz”, “4”, “Buzz”, “Fizz”, “7”, “8”, “Fizz”, “Buzz”, “11”, “Fizz”, “13”, “14”, “FizzBuzz”,
+“16”, “17”, “Fizz”, “19”, “Buzz”]
 *************************************************************************************************************/
+
 class Solution {
     public int countPrimes(int n) {
         int count=0;
