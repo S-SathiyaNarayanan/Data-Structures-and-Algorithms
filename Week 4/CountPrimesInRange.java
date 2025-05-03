a4f24c45b974ec8de57a325bdfb618dd77840a20
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-03 20:54:51 +0530

    Update CountPrimesInRange.java

--- Week 4/CountPrimesInRange.java
+++ Week 4/CountPrimesInRange.java
@@ -8,7 +8,7 @@ Output: 2 3 5 7
 Explanation: Prime numbers less than equal to 10 are 2 3 5 and 7
 *************************************************************************************************************/
 
-// Solution 1 - 
+// Solution 1 - Sieve of Eratosthenes O(nloglogn)
 class Solution {
     int countPrimes(int L, int R) {
         int count=0;
