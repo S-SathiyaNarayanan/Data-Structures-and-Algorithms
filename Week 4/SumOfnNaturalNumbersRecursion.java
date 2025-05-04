4f79b23d6f5a412813814647a346abb76df7617f
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-05 02:41:22 +0530

    Update SumOfnNaturalNumbersRecursion.java

--- Week 4/SumOfnNaturalNumbersRecursion.java
+++ Week 4/SumOfnNaturalNumbersRecursion.java
@@ -1,1 +1,13 @@
 
+import java.util.Scanner;
+static void SumOfN(int n){
+    
+}
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n=sc.nextInt();
+        SumOfN(n);
+    }
+}
