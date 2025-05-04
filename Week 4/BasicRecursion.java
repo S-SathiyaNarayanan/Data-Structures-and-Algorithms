345ee176c755dcd74b30f2ab50f6d3668e67970f
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-04 11:08:09 +0530

    Update BasicRecursion.java

--- Week 4/BasicRecursion.java
+++ Week 4/BasicRecursion.java
@@ -1,1 +1,13 @@
-
+public class BasicRecursion {
+    public static void printNumbers(int n) {
+        if (n <= 0) {
+            return; // Base case: stop recursion
+        } else {
+            System.out.println(n); // Print current number
+            printNumbers(n - 1);   // Recursive call
+        }
+    }
+    public static void main(String[] args) {
+        printNumbers(5); 
+    }
+}
