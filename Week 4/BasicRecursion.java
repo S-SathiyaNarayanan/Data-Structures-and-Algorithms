64da2a34a45c3de0ea63b4f5625474c9d510f93a
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-05 00:35:26 +0530

    Update BasicRecursion.java

--- Week 4/BasicRecursion.java
+++ Week 4/BasicRecursion.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 public class BasicRecursion {
     public static void printNumbers(int n) {    // Recursive method to print numbers from n to 1
         if (n == 0) {
-            return;                   // Base case: when n reaches 0, stop recursion
+            return;                     // Base case: when n reaches 0, stop recursion
         } else {
             System.out.print(n+" ");    // Print the current value of n
-            printNumbers(n - 1);      // Recursive call with n decremented by 1
+            printNumbers(n - 1);        // Recursive call with n decremented by 1
         }
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the value of n: ");
         int n=sc.nextInt();
-        printNumbers(n);              // Initial call to start recursion with n = 5
+        printNumbers(n);                // Initial call to start recursion with n = 5
     }
 }
