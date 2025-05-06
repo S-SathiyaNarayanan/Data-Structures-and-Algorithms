f23c0e76924e03ba14e5c711dc30b533d120e05c
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-06 18:38:19 +0530

    Update BinarySearch.java

--- Week 6/BinarySearch.java
+++ Week 6/BinarySearch.java
@@ -7,3 +7,19 @@ Input: nums = [-1,0,3,5,9,12], target = 9
 Output: 4
 Explanation: 9 exists in nums and its index is 4
 *************************************************************************************************************/
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int L=0;
+        int R=nums.length-1;
+        while(L<=R){
+            int mid=L+((R-L)/2);
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) L=mid+1;
+            else R=mid-1;
+        }
+        return -1;
+    }
+}
+
+
