4d8e24a0d6715e6ca1ae4428d42bb5bf8cac1f5e
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-06 19:31:10 +0530

    Update BinarySearch.java

--- Week 6/BinarySearch.java
+++ Week 6/BinarySearch.java
@@ -8,12 +8,19 @@ Output: 4
 Explanation: 9 exists in nums and its index is 4
 *************************************************************************************************************/
 
+// STANDARD BINARY SEARCH - Time Complexity O(log n) , Space Complexity O(1)
+// <<< Calculate mid index to prevent integer overflow >>>
+// First, it computes (right - left) this won’t overflow, because right is always >= left in a valid binary search
+// Then it divides by 2
+// Finally, it adds left.
+// This avoids adding two potentially large numbers, and thus prevents overflow.
 class Solution {
     public int search(int[] nums, int target) {
         int Left = 0;                   // Left pointer is the index at the start of the array
         int Right = nums.length - 1;    // Right pointer is the index at the end of the array
         while (Left <= Right) {                       // Repeat the steps while the search range is valid
-            int Mid = Left + ((Right - Left) / 2);    // Find the middle index between Left and Right ( Calculate mid index to prevent integer overflow )
+            // Efficient Mid Calculation prevents integer overflow, which can happen with (Left + Right)/2 when values are large
+            int Mid = Left + ((Right - Left) / 2);    // Find the middle index between Left and Right 
             if (nums[Mid] == target) return Mid;      // If value at middle index is target, return Mid
             else if (nums[Mid] < target)              // If value at Mid is smaller than target 
                 Left = Mid + 1;                       // search in the right half ( move left pointer )
