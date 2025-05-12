82b4178d7bd083d3fafd13963415f7fadb4e7a09
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-12 09:50:39 +0530

    Update BinarySearchLowerBound.java

--- Week 6/BinarySearchLowerBound.java
+++ Week 6/BinarySearchLowerBound.java
@@ -29,3 +29,20 @@ class Solution {
         return L;
     }
 }
+
+class Solution {
+    public int lowerBound(int[] arr, int target) {
+        int L = 0;             // Left pointer is the index at the start of the array
+        int R = arr.length;    // Right pointer is set to one past the last index (because we're searching for a lower bound)
+        
+        while (L < R) {                   // Repeat the process while L is less than R
+            int mid = L + (R - L) / 2;    // Efficient mid calculation to avoid integer overflow
+            if (arr[mid] < target) {      // If the value at mid is smaller than the target, move the left pointer
+                L = mid + 1;         
+            } else {                      // If the value at mid is greater than or equal to target, move the right pointer
+                R = mid;
+            }
+        }
+        return L;    // When L equals R, L will be the first index where arr[L] >= target
+    }
+}
