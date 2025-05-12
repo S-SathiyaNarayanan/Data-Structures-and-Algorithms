a6d5a0336723dc953102c9cea3cda54067476af4
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-12 08:49:51 +0530

    Update BinarySearchLowerBound.java

--- Week 6/BinarySearchLowerBound.java
+++ Week 6/BinarySearchLowerBound.java
@@ -1,7 +1,11 @@
 /*************************************************************************************************************
-🪙GeeksforGeeks - Count Primes In Range
-Given two numbers L and R(L<R). Count all the prime numbers in the range [L, R]
-Input : L=1,R=10
-Output: 4
-Explanation : There are 4 primes in this range, which are 2,3,5 and 7
+🪙GeeksforGeeks - Implement Lower Bound
+Given sorted array arr[] and a number target,the task is to find the lower bound of the target in this given
+array. The lower bound of a number is defined as the smallest index in the sorted array where the element is 
+greater than or equal to the given number.
+Note: If all the elements in the given array are smaller than the target, the lower bound will be the length 
+of the array. 
+Input:  arr[] = [2, 3, 7, 10, 11, 11, 25], target = 9
+Output: 3
+Explanation: 3 is the smallest index in arr[] where element (arr[3] = 10) is greater than or equal to 9
 *************************************************************************************************************/
