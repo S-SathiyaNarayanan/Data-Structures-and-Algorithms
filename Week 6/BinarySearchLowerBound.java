c07822bbecc3908bc4e759791e8da3cd7ba6a1c7
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-12 08:46:32 +0530

    Update BinarySearchLowerBound.java

--- Week 6/BinarySearchLowerBound.java
+++ Week 6/BinarySearchLowerBound.java
@@ -1,1 +1,7 @@
-
+/*************************************************************************************************************
+🪙GeeksforGeeks - Count Primes In Range
+Given two numbers L and R(L<R). Count all the prime numbers in the range [L, R]
+Input : L=1,R=10
+Output: 4
+Explanation : There are 4 primes in this range, which are 2,3,5 and 7
+*************************************************************************************************************/
