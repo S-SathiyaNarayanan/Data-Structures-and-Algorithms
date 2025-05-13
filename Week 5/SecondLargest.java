91db0e49bd38970189b37f565b7d610b329759cc
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-13 13:51:29 +0530

    Update SecondLargest.java

--- Week 5/SecondLargest.java
+++ Week 5/SecondLargest.java
@@ -9,7 +9,6 @@ Explanation: The largest element of the array is 35 and the second largest eleme
 
 class Solution {
     public int getSecondLargest(int[] arr) {
-        // code here
         int FL=-1;
         int SL=-1;
         for(int i=0;i<arr.length;i++){
