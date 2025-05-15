2a05de107989aca2fa9a31be61674a65dd3ea5f0
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-16 05:20:44 +0530

    Update ThirdLargestElement.java

--- Week 5/ThirdLargestElement.java
+++ Week 5/ThirdLargestElement.java
@@ -6,3 +6,26 @@ Input: arr[] = [2, 4, 1, 3, 5]
 Output: 3
 Explanation: The third largest element in the array [2, 4, 1, 3, 5] is 3.
 *************************************************************************************************************/
+
+class Solution {
+    int thirdLargest(int arr[]) {
+        // Your code here
+        if(arr.length<=2) return -1;
+        int FL=Integer.MIN_VALUE;
+        int SL=Integer.MIN_VALUE;
+        int TL=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>FL){
+                TL=SL;
+                SL=FL;
+                FL=arr[i];
+            }else if(arr[i]>SL){
+                TL=SL;
+                SL=arr[i];
+            }else if(arr[i]>TL){
+                TL=arr[i];
+            }
+        }
+        return TL;
+    }
+}
