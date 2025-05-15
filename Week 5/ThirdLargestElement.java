7eecc6752e955c2a7890a5f47dd0a6543c6133d6
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-05-16 05:26:43 +0530

    Update ThirdLargestElement.java

--- Week 5/ThirdLargestElement.java
+++ Week 5/ThirdLargestElement.java
@@ -9,22 +9,30 @@ Explanation: The third largest element in the array [2, 4, 1, 3, 5] is 3.
 
 class Solution {
     int thirdLargest(int arr[]) {
-        if(arr.length<=2) return -1;
-        int FL=Integer.MIN_VALUE;
-        int SL=Integer.MIN_VALUE;
-        int TL=Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>FL){
-                TL=SL;
-                SL=FL;
-                FL=arr[i];
-            }else if(arr[i]>SL){
-                TL=SL;
-                SL=arr[i];
-            }else if(arr[i]>TL){
-                TL=arr[i];
+        if (arr.length <= 2) return -1;    // If there are fewer than 3 elements, third largest doesn't exist
+        // Initialize first, second, and third largest values to minimum integer value
+        int FL = Integer.MIN_VALUE;
+        int SL = Integer.MIN_VALUE;
+        int TL = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > FL) {    // If current element is greater than first largest
+                TL = SL;
+                SL = FL;
+                FL = arr[i];      // Update third and second largest before updating first largest
+            }
+            
+            else if (arr[i] > SL && arr[i] != FL) {    // If current element is not the largest but greater than second largest
+                // Update third largest before updating second largest
+                TL = SL;
+                SL = arr[i];
+            }
+            // If current element is not among the top two but greater than third largest
+            else if (arr[i] > TL && arr[i] != SL && arr[i] != FL) {
+                TL = arr[i];
             }
         }
+        // Return third largest value found
         return TL;
     }
 }
+
