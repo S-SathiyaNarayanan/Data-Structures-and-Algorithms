f3f884f8ebdea776caea1cfb8ca8ed8f0e42f136
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-06-03 21:32:11 +0530

    Update SquarePattern.java

--- Week 2/SquarePattern.java
+++ Week 2/SquarePattern.java
@@ -1,3 +1,16 @@
+/*************************************************************************************************************
+🪙GeeksforGeeks - Pattern Printing
+Given a number N. The task is to print a series of asterisk(*) from 1 till N terms with increasing order and 
+difference being 1.
+Input:
+N = 3
+Output:
+* ** ***
+Explanation:
+First, print 1 asterisk then space after that print 2 asterisk and space after that  print 3 asterisk now 
+stop as N is 3.
+*************************************************************************************************************/
+
 class Solution {
     void printSquare(int n) {
         for(int i = 1; i <= n; i++) {
