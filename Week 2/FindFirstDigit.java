eec632e34a8d2cc2b6ad42b86bd7cbbe2ef9bff4
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-06-04 02:59:30 +0530

    Update FindFirstDigit.java

--- Week 2/FindFirstDigit.java
+++ Week 2/FindFirstDigit.java
@@ -4,15 +4,12 @@ public class FindFirstDigit {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
         int N = sc.nextInt();
-
         int FirstDigit = N;       // Initialize FirstDigit with the given number
         int LastDigit = N % 10;   // Extract the last digit using modulo operation
-
         // Loop to extract the first digit by continuously dividing by 10
         while (FirstDigit >= 10) {          // Loop runs until only one digit remains
             FirstDigit = FirstDigit / 10;   // Remove the last digit in each iteration
         }
-
         System.out.println("The first digit is " + FirstDigit);
         System.out.println("The last digit is " + LastDigit);
     }
