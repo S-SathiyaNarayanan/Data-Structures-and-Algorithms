00ed4ec7e622765428ba29c1d4aaff13ac1a829a
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-06-03 21:38:44 +0530

    Update SquarePattern.java

--- Week 2/SquarePattern.java
+++ Week 2/SquarePattern.java
@@ -3,12 +3,16 @@
 Geek is very fond of patterns. Once, his teacher gave him a square pattern to solve. He gave Geek an integer n
 and asked him to build a pattern. 
 Input:
-N = 3
+n = 5
 Output:
-* ** ***
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
 Explanation:
-First, print 1 asterisk then space after that print 2 asterisk and space after that  print 3 asterisk now 
-stop as N is 3.
+You don't need to input anything. Complete the function printSquare() which takes  an integer n  as the input
+parameter and print the pattern.
 *************************************************************************************************************/
 
 class Solution {
