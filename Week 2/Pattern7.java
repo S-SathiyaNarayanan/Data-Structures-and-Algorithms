d41bdd6ec7d085378ada7d2cb636427c1c0fe612
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-06-04 19:03:59 +0530

    Update Pattern7.java

--- Week 2/Pattern7.java
+++ Week 2/Pattern7.java
@@ -12,15 +12,12 @@ Expected output:
 public class Pattern7 {
     public static void main(String[] args) {
         int n = 4;  // Define the number of rows for the pattern
-      
         // Outer loop: Controls the number of rows, starting from 'n' down to 1
         for (int i = n; i >= 1; i--) {
-
             // Inner loop: Prints numbers from 'i' down to 1 in each row
             for (int j = i; j >= 1; j--) {
                 System.out.print(j + " ");  // Print the current number with a space
             }
-
             System.out.println();  // Move to the next line after printing one row
         }
     }
