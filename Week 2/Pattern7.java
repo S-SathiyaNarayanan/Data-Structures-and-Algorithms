923e70595480ba8b037b607890bc41291a75a602
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-06-04 19:07:07 +0530

    Update Pattern7.java

--- Week 2/Pattern7.java
+++ Week 2/Pattern7.java
@@ -25,4 +25,17 @@ public class Pattern7 {
 }
 
 // Solution 2
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >= i; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
 
