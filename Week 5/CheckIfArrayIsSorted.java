404a53e5e845f2d2da85b39e4c719874b99a1e97
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-06-09 13:49:49 +0530

    Update CheckIfArrayIsSorted.java

--- Week 5/CheckIfArrayIsSorted.java
+++ Week 5/CheckIfArrayIsSorted.java
@@ -2,9 +2,7 @@ class Solution {
     public boolean arraySortedOrNot(int[] arr) {
         if(arr.length==1) return true;
         for(int i=1;i<arr.length;i++){
-            if(arr[i-1]>arr[i]){
-                return false;
-            }
+            if(arr[i]<arr[i-1]) return false;
         }
         return true;
     }
