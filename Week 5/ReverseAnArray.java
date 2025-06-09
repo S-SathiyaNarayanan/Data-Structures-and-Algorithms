63781786e3521a4b9b6288b10f9d600fc8cc74a0
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-06-09 23:11:57 +0530

    Update ReverseAnArray.java

--- Week 5/ReverseAnArray.java
+++ Week 5/ReverseAnArray.java
@@ -1,10 +1,9 @@
 /*************************************************************************************************************
 🪙GeeksforGeeks - Reverse an Array
-Given an array arr[], check whether it is sorted in non-decreasing order. Return true if it is sorted otherwise
-false.
-Input: arr[] = [10, 20, 30, 40, 50]
-Output: true
-Explanation: The given array is sorted.
+You are given an array of integers arr[]. Your task is to reverse the given array.
+Input: arr = [1, 4, 3, 2, 6, 5]
+Output: [5, 6, 2, 3, 4, 1]
+Explanation: Modify the array in place.
 *************************************************************************************************************/
 class Solution {
     public void reverseArray(int arr[]) {
