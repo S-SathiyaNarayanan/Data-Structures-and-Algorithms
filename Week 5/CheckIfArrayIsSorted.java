0787d9be5457c6794bf0775cbe9bd6fa5b6dd7fc
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-06-09 14:01:08 +0530

    Update CheckIfArrayIsSorted.java

--- Week 5/CheckIfArrayIsSorted.java
+++ Week 5/CheckIfArrayIsSorted.java
@@ -6,11 +6,26 @@ Input: arr[] = [10, 20, 30, 40, 50]
 Output: true
 Explanation: The given array is sorted.
 *************************************************************************************************************/
+
+// Solution 1
 class Solution {
     public boolean arraySortedOrNot(int[] arr) {
         if(arr.length==1) return true;
         for(int i=1;i<arr.length;i++){
             if(arr[i]<arr[i-1]) return false;
+        }
+        return true;
+    }
+}
+
+// Solution 2
+class Solution {
+    public boolean arraySortedOrNot(int[] arr) {
+        if(arr.length==1) return true;
+        int i=0;
+        for(int j=1;j<arr.length;j++){
+            if(arr[i]>arr[j]) return false;
+            i++;
         }
         return true;
     }
