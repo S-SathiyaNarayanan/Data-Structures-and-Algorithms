3fecb5b950ce526266b1475049a67d90a33be92e
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-06-10 12:58:06 +0530

    Update RemoveDuplicatesFromSortedArray.java

--- Week 5/RemoveDuplicatesFromSortedArray.java
+++ Week 5/RemoveDuplicatesFromSortedArray.java
@@ -1,9 +1,12 @@
 class Solution {
     public int removeDuplicates(int[] arr) {
+        // Initialize the New Index (NI) to 1, since the first element is always unique
         int NI = 1;
+        // Start looping from the second element (index 1) to the end of the array
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i] != arr[i - 1]) {
-                arr[NI] = arr[i];
+            if (arr[i] != arr[i - 1]) {    // Compare current element with previous element
+                // If current element is not equal to previous element, it's a unique element
+                arr[NI] = arr[i];    // Assign it to the NI position in the array
                 NI++;
             }
         }
