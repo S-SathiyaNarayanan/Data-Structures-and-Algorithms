e4d7545b43f1706b4177563b731881c9178dfd8f
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-06-10 13:04:58 +0530

    Update RemoveDuplicatesFromSortedArray.java

--- Week 5/RemoveDuplicatesFromSortedArray.java
+++ Week 5/RemoveDuplicatesFromSortedArray.java
@@ -1,3 +1,11 @@
+/*************************************************************************************************************
+🪙GeeksforGeeks - Reverse an Array
+You are given an array of integers arr[]. Your task is to reverse the given array.
+Input: arr = [1, 4, 3, 2, 6, 5]
+Output: [5, 6, 2, 3, 4, 1]
+Explanation: Modify the array in place.
+*************************************************************************************************************/
+
 class Solution {
     public int removeDuplicates(int[] arr) {
         // Initialize NI (New Index) to 1 because the first element is always considered unique
