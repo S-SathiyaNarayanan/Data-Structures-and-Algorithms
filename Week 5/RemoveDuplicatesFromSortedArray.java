0f0a701d7a254a351610a79fffca92b13742a416
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-06-10 12:43:34 +0530

    Update RemoveDuplicatesFromSortedArray.java

--- Week 5/RemoveDuplicatesFromSortedArray.java
+++ Week 5/RemoveDuplicatesFromSortedArray.java
@@ -1,1 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] arr) {
+        int NI = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                arr[NI] = arr[i];
+                NI++;
+            }
+        }
+        return NI;
+    }
+}
 
