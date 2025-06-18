cf268d283e941bae087b32004a170fe2e3545e6b
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-06-18 19:37:15 +0530

    Update SelectionSort.java

--- Week 4/SelectionSort.java
+++ Week 4/SelectionSort.java
@@ -7,3 +7,19 @@ Explanation: Maintain sorted (in bold) and unsorted subarrays. Select 1. Array b
 Array becomes 1 3 4 9 7. Select 4. Array becomes 1 3 4 9 7. Select 7. Array becomes 1 3 4 7 9. Select 9. Array 
 becomes 1 3 4 7 9.
 *************************************************************************************************************/
+
+class Solution {
+    void selectionSort(int[] arr) {
+        // code here
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            int MI=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[MI]) MI=j;
+            }
+            int temp=arr[MI];
+            arr[MI]=arr[i];
+            arr[i]=temp;
+        }
+    }
+}
