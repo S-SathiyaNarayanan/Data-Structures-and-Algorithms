4dbbd0048ebba564916da1bdc2cdb21e513a5699
Sathiya Narayanan <84930304+S-SathiyaNarayanan@users.noreply.github.com>
2025-06-18 19:36:57 +0530

    Update SelectionSort.java

--- Week 4/SelectionSort.java
+++ Week 4/SelectionSort.java
@@ -1,1 +1,9 @@
-
+/*************************************************************************************************************
+🪙GeeksforGeeks - Selection Sort
+Given an array arr, use selection sort to sort arr[] in increasing order.
+Input: arr[] = [4, 1, 3, 9, 7]
+Output: [1, 3, 4, 7, 9]
+Explanation: Maintain sorted (in bold) and unsorted subarrays. Select 1. Array becomes 1 4 3 9 7. Select 3. 
+Array becomes 1 3 4 9 7. Select 4. Array becomes 1 3 4 9 7. Select 7. Array becomes 1 3 4 7 9. Select 9. Array 
+becomes 1 3 4 7 9.
+*************************************************************************************************************/
